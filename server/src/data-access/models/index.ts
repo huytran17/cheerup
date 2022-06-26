4561f5d1348556d71ba8cfa2e9a30adf1e4a48e6
huytran17 <alpha.lloyd1368@gmail.com>
2022-06-26 14:02:47 +0700

    admin model

--- server/src/data-access/models/index.ts
+++ server/src/data-access/models/index.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 import userSchema from "../../database/schemas/user";
+import adminSchema from "../../database/schemas/admin";
 
 import IUser from "../../database/interfaces/user";
+import IAdmin from "../../database/interfaces/admin";
 
 type IUserModel = IUser & mongoose.Document;
+type IAdminModel = IAdmin & mongoose.Document;
 
 // Models
 const UserModel = mongoose.model<IUserModel>("User", userSchema);
+const AdminModel = mongoose.model<IAdminModel>("Admin", adminSchema);
 
 export default Object.freeze({
   UserModel,
+  AdminModel,
 });
 
-export { UserModel };
+export { UserModel, AdminModel };
