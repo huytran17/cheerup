f373ae22bdf9634475375913aac0da8c33835c3a
huytran17 <alpha.lloyd1368@gmail.com>
2022-06-26 14:00:26 +0700

    admin interface

---
+++ server/src/database/interfaces/admin.ts
@@ -0,0 +1,10 @@
+export default interface IUser {
+  _id: string;
+  hash_password: string;
+  full_name: string;
+  aws_avatar?: Record<string, unknown>;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
