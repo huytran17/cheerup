b8b2373fc11fc3cc52dfb9daa29325de4f28651c
huytran17 <alpha.lloyd1368@gmail.com>
2022-06-26 14:00:30 +0700

    admin schema

---
+++ server/src/database/schemas/admin.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import _ from "lodash";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    hash_password: { type: String, trim: true },
+    aws_avatar: { type: Object },
+    email: { type: String, trim: true, lowercase: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+    deleted_at: { type: Date, default: null },
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
+
+userSchema.virtual("alias_name").get(function (this: { full_name: string }) {
+  const matches = this.full_name?.match(/\b(\w)/g); // ['J','S','O','N']
+
+  const acronym = matches?.join(""); // JSON
+
+  return acronym;
+});
+
+export default userSchema;
