a7327ec225c48ba2f819d5fd430817c07483271c
huytran17 <alpha.lloyd1368@gmail.com>
2022-06-26 13:56:25 +0700

    init server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,31 @@
 import express from "express";
-import helmet from "helmet";
+import makeDb from "./data-access/make-db";
+import { UserDb } from "./data-access";
 import cors from "cors";
 import bodyParser from "body-parser";
+import appRouter from "./routes";
+import passport from "./config/passport";
+import { upload } from "./config/middlewares/file-upload-middleware";
 
 const app = express();
 
-app.use(helmet());
-app.use(helmet.xssFilter());
-app.use(helmet.hidePoweredBy());
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(appRouter);
+app.use(upload.single("file"));
 
-app.use(cors());
+app.listen(3000, () => console.log("Server is listening on port 3000"));
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+makeDb().then(async () => {
+  const user = await UserDb.findOne();
+  if (!user) {
+    await UserDb.insert({
+      first_name: "Huy",
+      last_name: "Tran",
+      email: "huytran@gmail.com",
+      hash_password: "qwer1234",
+    });
+  }
 });
