521fb048ef0302846d984f784d11c2361ea9e1d2
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 16:24:44 +0700

    post db interface

---
+++ server/src/data-access/interfaces/post-db.ts
@@ -0,0 +1,32 @@
+import Post from "../../database/entities/post";
+import IPost from "../../database/interfaces/post";
+export default interface IPostDb {
+  findAll: () => Promise<Post[] | null>;
+  findAllPaginated: ({
+    query,
+    page,
+    entries_per_page,
+  }: {
+    query: string;
+    page: number;
+    entries_per_page?: number;
+  }) => Promise<PaginatedPostResult | null>;
+  findOne: () => Promise<Post | null>;
+  findById: ({ _id }: { _id: string }) => Promise<Post | null>;
+  insert: (payload: Partial<IPost>) => Promise<Post | null>;
+  delete: ({ _id }: { _id: string }) => Promise<Post | null>;
+  hardDelete: ({ _id }: { _id: string }) => Promise<Post | null>;
+  update: (updatePayload: Partial<IPost>) => Promise<Post | null>;
+}
+
+export interface PaginatedPostResult {
+  data: Post[];
+  pagination: {
+    current_page: number | null;
+    from: number | null;
+    to: number | null;
+    per_page: number | null;
+    total: number | null;
+    total_pages: number | null;
+  };
+}
