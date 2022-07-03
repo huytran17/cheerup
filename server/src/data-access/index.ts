b9d0e8b0f21a8b2d4b7663514efe31adc4788a82
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 15:19:33 +0700

    data access

--- server/src/data-access/index.ts
+++ server/src/data-access/index.ts
@@ -2,15 +2,24 @@ import moment from "moment";
 
 import makeUserDb from "./make-user-db";
 import makeAdminDb from "./make-admin-db";
+import makeCategoryDb from "./make-category-db";
+import makeCommentDb from "./make-comment-db";
 
-import { UserModel, AdminModel } from "./models";
+import { UserModel, AdminModel, CategoryModel, CommentModel } from "./models";
 
+const CommentDb = makeCommentDb({ commentDbModel: CommentModel, moment });
 const UserDb = makeUserDb({ userDbModel: UserModel, moment });
 const AdminDb = makeAdminDb({ adminDbModel: AdminModel, moment });
+const CategoryDb = makeCategoryDb({
+  categoryDbModel: CategoryModel,
+  moment,
+});
 
 export default Object.freeze({
   UserDb,
   AdminDb,
+  CategoryDb,
+  CommentDb,
 });
 
-export { UserDb, AdminDb };
+export { UserDb, AdminDb, CategoryDb, CommentDb };
