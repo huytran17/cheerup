e75bb33af9912c8de691dc6c4f2b17827b190827
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 14:35:18 +0700

    comment schema

---
+++ server/src/database/schemas/comment.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  content: { type: String, trim: true },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  post: { type: Schema.Types.ObjectId, ref: "Post" },
+  children: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  meta: { type: Object },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  deleted_at: { type: Date, default: null },
+});
