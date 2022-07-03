3031a58f5411b5eaf929943f2eb0375bff1beefd
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 17:40:22 +0700

    user validators

---
+++ server/src/data-access/controllers/admin/user/validators/create-user.ts
@@ -0,0 +1,6 @@
+const createUserRules = {
+  email: "required|string",
+  hash_password: "required|string",
+};
+
+export default createUserRules;
--- server/src/data-access/controllers/admin/user/validators/index.ts
+++ server/src/data-access/controllers/admin/user/validators/index.ts
@@ -1,11 +1,21 @@
 import getUserRules from "./get-user";
 import deleteUserRules from "./delete-user";
 import getUserByEmailRules from "./get-user-by-email";
+import createUserRules from "./create-user";
+import updateUserRules from "./update-user";
 
 export default Object.freeze({
   getUserRules,
   deleteUserRules,
   getUserByEmailRules,
+  createUserRules,
+  updateUserRules,
 });
 
-export { getUserRules, deleteUserRules, getUserByEmailRules };
+export {
+  getUserRules,
+  deleteUserRules,
+  getUserByEmailRules,
+  createUserRules,
+  updateUserRules,
+};
