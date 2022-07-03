0460798671b6afc7ca33d11eddbc572eb78f651b
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 14:03:37 +0700

    post model

--- server/src/data-access/models/index.ts
+++ server/src/data-access/models/index.ts
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 import userSchema from "../../database/schemas/user";
 import adminSchema from "../../database/schemas/admin";
+import postSchema from "../../database/schemas/post";
 
 import IUser from "../../database/interfaces/user";
 import IAdmin from "../../database/interfaces/admin";
+import IPost from "../../database/interfaces/post";
 
 type IUserModel = IUser & mongoose.Document;
 type IAdminModel = IAdmin & mongoose.Document;
+type IPostModel = IPost & mongoose.Document;
 
 // Models
 const UserModel = mongoose.model<IUserModel>("User", userSchema);
 const AdminModel = mongoose.model<IAdminModel>("Admin", adminSchema);
+const PostModel = mongoose.model<IPostModel>("Post", postSchema);
 
 export default Object.freeze({
   UserModel,
   AdminModel,
+  PostModel,
 });
 
-export { UserModel, AdminModel };
+export { UserModel, AdminModel, PostModel };
