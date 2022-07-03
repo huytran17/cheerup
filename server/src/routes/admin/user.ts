d3b11540ae610c006ccc077968d98c30cc8dfa3d
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 18:09:41 +0700

    admin user routes

--- server/src/routes/admin/user.ts
+++ server/src/routes/admin/user.ts
@@ -4,25 +4,25 @@ import makeExpressCallback from "../../config/express-callback";
 
 import {
   getUserRules,
-  // updateUserRules,
-  // deleteUserRules,
-  // createUserRules,
+  updateUserRules,
+  deleteUserRules,
+  createUserRules,
 } from "../../data-access/controllers/admin/user/validators";
 import {
   getUserController,
-  // updateUserController,
-  // getUsersController,
-  // deleteUserController,
-  // createUserController,
+  updateUserController,
+  getUsersController,
+  deleteUserController,
+  createUserController,
 } from "../../data-access/controllers/admin/user";
 
 const userRouter = express.Router();
 
-// userRouter.delete(
-//   "/delete/:user_id",
-//   makeValidator(deleteUserRules),
-//   makeExpressCallback(deleteUserController)
-// );
+userRouter.delete(
+  "/delete/:user_id",
+  makeValidator(deleteUserRules),
+  makeExpressCallback(deleteUserController)
+);
 
 /**
  * @openapi
@@ -47,18 +47,19 @@ userRouter.get(
   makeValidator(getUserRules),
   makeExpressCallback(getUserController)
 );
-// userRouter.post(
-//   "/",
-//   makeValidator(createUserRules),
-//   makeExpressCallback(createUserController)
-// );
 
-// userRouter.put(
-//   "/",
-//   makeValidator(updateUserRules),
-//   makeExpressCallback(updateUserController)
-// ); // DONE
+userRouter.post(
+  "/",
+  makeValidator(createUserRules),
+  makeExpressCallback(createUserController)
+);
 
-// userRouter.get("/", makeExpressCallback(getUsersController)); // DONE
+userRouter.put(
+  "/",
+  makeValidator(updateUserRules),
+  makeExpressCallback(updateUserController)
+); // DONE
+
+userRouter.get("/", makeExpressCallback(getUsersController)); // DONE
 
 export default userRouter;
