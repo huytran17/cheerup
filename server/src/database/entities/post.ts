e4575d56ca7923c79104ff3d4ce65c908eb8ddda
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 16:25:01 +0700

    post entity

--- server/src/database/entities/post.ts
+++ server/src/database/entities/post.ts
@@ -14,4 +14,30 @@ export default class Post implements IPost {
   public readonly created_at: Date;
   public readonly updated_at: Date;
   public readonly deleted_at: Date;
+
+  constructor({
+    _id,
+    title,
+    description,
+    thumbnail,
+    content,
+    author,
+    category,
+    meta,
+    created_at,
+    updated_at,
+    deleted_at,
+  }) {
+    this._id = _id;
+    this.title = title;
+    this.description = description;
+    this.thumbnail = thumbnail;
+    this.content = content;
+    this.author = author;
+    this.category = category;
+    this.meta = meta;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.deleted_at = deleted_at;
+  }
 }
