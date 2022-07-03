64df30d463ae4d6eb5c817144506009c9680c62a
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 15:18:39 +0700

    comment interface db

---
+++ server/src/data-access/interfaces/comment-db.ts
@@ -0,0 +1,31 @@
+import Comment from "../../database/entities/comment";
+import IComment from "../../database/interfaces/comment";
+export default interface ICommentDb {
+  findAllPaginated: ({
+    query,
+    page,
+    entries_per_page,
+  }: {
+    query: string;
+    page: number;
+    entries_per_page?: number;
+  }) => Promise<PaginatedCommentResult | null>;
+  findOne: () => Promise<Comment | null>;
+  findById: ({ _id }: { _id: string }) => Promise<Comment | null>;
+  insert: (payload: Partial<IComment>) => Promise<Comment | null>;
+  delete: ({ _id }: { _id: string }) => Promise<Comment | null>;
+  hardDelete: ({ _id }: { _id: string }) => Promise<Comment | null>;
+  update: (updatePayload: Partial<IComment>) => Promise<Comment | null>;
+}
+
+export interface PaginatedCommentResult {
+  data: Comment[];
+  pagination: {
+    current_page: number | null;
+    from: number | null;
+    to: number | null;
+    per_page: number | null;
+    total: number | null;
+    total_pages: number | null;
+  };
+}
