e5d65ad14467d2322c9767ce80acc47a19ec3889
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-03 17:21:46 +0700

    admin post validators

---
+++ server/src/data-access/controllers/admin/post/validators/get-post.ts
@@ -0,0 +1,5 @@
+const getCommentRules = {
+  _id: "required|string",
+};
+
+export default getCommentRules;
---
+++ server/src/data-access/controllers/admin/post/validators/index.ts
@@ -0,0 +1,11 @@
+import getCommentRules from "./get-post";
+import deleteCommentRules from "./delete-post";
+import updateCommentRules from "./update-post";
+
+export default Object.freeze({
+  getCommentRules,
+  deleteCommentRules,
+  updateCommentRules,
+});
+
+export { getCommentRules, deleteCommentRules, updateCommentRules };
