dcd2387029d2ee27b363e3a495e733f037d8fa03
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-23 22:38:16 +0700

    auth mixin

--- core/user-dashboard/mixins/auth.js
+++ core/user-dashboard/mixins/auth.js
@@ -2,6 +2,27 @@ import _ from "lodash";
 import { mapActions, mapMutations, mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      emailRules: [
+        (v) => !!v || this.$t("E-mail is required."),
+        (v) => /.+@.+\..+/.test(v) || this.$t("E-mail must be valid."),
+      ],
+      passwordRules: [
+        (v) => !!v || this.$t("Password is required."),
+        (v) =>
+          (v && v.length > 7) || this.$t("Password must be min 8 characters."),
+      ],
+      passwordConfirmationRules: [
+        (v) => !!v || this.$t("Password confirmation is required."),
+        (v) =>
+          (v && v.length > 7) || this.$t("Password must be min 8 characters."),
+        (v) =>
+          this.user.password === v ||
+          this.$t("Password confirmation must match password."),
+      ],
+    };
+  },
   computed: {
     ...mapGetters({
       me: "auth/me",
