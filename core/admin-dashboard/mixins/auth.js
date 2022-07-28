2a1edad69a03ce5621a83a095b4b4fd430b9804f
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-28 20:26:40 +0700

    auth mixin

---
+++ core/admin-dashboard/mixins/auth.js
@@ -0,0 +1,49 @@
+import _ from "lodash";
+import { mapActions, mapMutations, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      emailRules: [
+        (v) => !!v || this.$t("E-mail is required."),
+        (v) => /.+@.+\..+/.test(v) || this.$t("E-mail must be valid."),
+      ],
+      passwordRules: [
+        (v) => !!v || this.$t("Password is required."),
+        (v) =>
+          (v && v.length > 7) || this.$t("Password must be min 8 characters."),
+      ],
+      passwordConfirmationRules: [
+        (v) => !!v || this.$t("Password confirmation is required."),
+        (v) =>
+          (v && v.length > 7) || this.$t("Password must be min 8 characters."),
+        (v) =>
+          this.user.password === v ||
+          this.$t("Password confirmation must match password."),
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters({
+      me: "auth/me",
+      user: "auth/user",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      SIGN_IN: "auth/SIGN_IN",
+      SIGN_OUT: "auth/SIGN_OUT",
+    }),
+    ...mapMutations({
+      SET_USER: "auth/SET_USER",
+      UPDATE_USER_DATA: "auth/UPDATE_USER_DATA",
+    }),
+
+    updateUserObject({ variable_path, data }) {
+      this.UPDATE_USER_DATA({
+        variable_path,
+        data,
+      });
+    },
+  },
+};
