db6a10c282cfc296716ef016e203a4ab6a2d2557
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-28 20:21:57 +0700

    add pkgs

--- core/admin-dashboard/nuxt.config.js
+++ core/admin-dashboard/nuxt.config.js
@@ -23,6 +23,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
+  server: {
+    port: 8080,
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
