cdcb368f89e1be08c61aea83bf13e7f087c067c8
huytran17 <alpha.lloyd1368@gmail.com>
2022-07-31 19:31:01 +0700

    admin nav

--- core/admin-dashboard/components/SideNavListItems.vue
+++ core/admin-dashboard/components/SideNavListItems.vue
@@ -1,38 +1,51 @@
 <template>
   <div class="nav-items d-flex flex-column">
-    <div
-      v-for="(item, index) in nav_items"
-      :key="index"
-      class="nav-item"
-      :class="[isExpandable(item) ? 'nav-item-collapse' : '']"
-      @click.stop.prevent="
-        (event) =>
-          toggleExpandNavItem({ expandable: isExpandable(item), event })
-      "
-    >
-      <div class="d-flex clickable" @click="$router.push(localePath(item.to))">
+    <div v-for="(item, index) in nav_items" :key="index">
+      <v-expand-transition>
         <div
-          v-if="item.header"
-          class="text-body-1 text-uppercase non-clickable nav-item-header py-3 position-relative pl-11 side-nav-header--text"
+          class="nav-item"
+          :class="[isExpandable(item) ? 'nav-item-collapse expandable' : '']"
+          @click.stop.prevent="
+            (event) => {
+              toggleExpandNavItem({ expandable: isExpandable(item), event });
+            }
+          "
         >
-          <span class="app-body" v-html="$t(item.header)"></span>
-        </div>
-        <div class="d-flex justify-between w-100 px-2">
-          <div class="text-body-1 clickable" :style="nav_spacing_style">
-            <div>
-              <span class="app-body" v-html="$t(item.title)"></span>
+          <div
+            class="d-flex clickable"
+            @click="$router.push(localePath(item.to))"
+          >
+            <div
+              v-if="item.header"
+              class="text-body-1 text-uppercase non-clickable nav-item-header py-3 position-relative pl-11 side-nav-header--text"
+            >
+              <span class="app-body" v-html="$t(item.header)"></span>
+            </div>
+            <div
+              class="d-flex justify-between w-100 px-2 nav-item-hover nav-item-activable py-2"
+              :class="[
+                isExpandable(item)
+                  ? 'nav-expansion-activable'
+                  : 'nav-non-expandable',
+              ]"
+            >
+              <div class="text-body-1 clickable" :style="nav_spacing_style">
+                <div>
+                  <span class="app-body" v-html="$t(item.title)"></span>
+                </div>
+              </div>
+              <v-icon v-if="isExpandable(item)" class="mr-3"
+                >mdi-chevron-right</v-icon
+              >
             </div>
           </div>
-          <v-icon v-if="isExpandable(item)" class="mr-3"
-            >mdi-chevron-right</v-icon
-          >
+          <SideNavListItems
+            v-if="hasChildren(item)"
+            :nav_items="item.children"
+            :item_spacing="item_spacing + 20"
+          />
         </div>
-      </div>
-      <SideNavListItems
-        v-if="hasChildren(item)"
-        :nav_items="item.children"
-        :item_spacing="item_spacing + 20"
-      />
+      </v-expand-transition>
     </div>
   </div>
 </template>
@@ -95,7 +108,25 @@ export default {
                     title: "Categories",
                     to: "/categories",
                     icon: "mdi-account-group-outline",
+                    expandable: true,
                     roles: [ADMIN_ROLES.SUPER_ADMIN, ADMIN_ROLES.ADMIN],
+                    children: [
+                      {
+                        title: "Categories",
+                        to: "/categories",
+                        icon: "mdi-account-group-outline",
+                        expandable: true,
+                        roles: [ADMIN_ROLES.SUPER_ADMIN, ADMIN_ROLES.ADMIN],
+                        children: [
+                          {
+                            title: "Categories",
+                            to: "/categories",
+                            icon: "mdi-account-group-outline",
+                            roles: [ADMIN_ROLES.SUPER_ADMIN, ADMIN_ROLES.ADMIN],
+                          },
+                        ],
+                      },
+                    ],
                   },
                 ],
               },
@@ -187,6 +218,9 @@ export default {
       },
     },
   },
+  data() {
+    return {};
+  },
   computed: {
     nav_spacing_style() {
       return {
@@ -207,23 +241,58 @@ export default {
     },
 
     toggleExpandNavItem({ expandable, event }) {
+      const current_target = event.currentTarget;
+
+      const hover_items = document.querySelectorAll(".nav-item-activable");
+
+      hover_items.forEach((item) => {
+        item.classList.remove("nav-item-active");
+        item.classList.add("nav-item-hover");
+      });
+
+      const nav_non_expandable_active_items = document.querySelectorAll(
+        ".nav-non-expandable-active"
+      );
+      nav_non_expandable_active_items.forEach((item) => {
+        item.classList.remove("nav-item-hover");
+      });
+
       if (!expandable) {
+        const nav_non_expandable_items = document.querySelectorAll(
+          ".nav-non-expandable"
+        );
+
+        nav_non_expandable_items.forEach((item) => {
+          item.classList.remove("nav-non-expandable-active");
+        });
+
+        const closest_expandable_item = current_target.closest(".expandable");
+        const child_expansion_activable_item =
+          closest_expandable_item.querySelector(".nav-expansion-activable");
+        child_expansion_activable_item.classList.add("nav-item-active");
+        child_expansion_activable_item.classList.remove("nav-item-hover");
+
+        const child_nav_non_expandable = current_target.querySelector(
+          ".nav-non-expandable"
+        );
+
+        child_nav_non_expandable.classList.add("nav-non-expandable-active");
+        child_nav_non_expandable.classList.remove("nav-item-hover");
+
         return;
       }
 
-      const current_target = event.currentTarget;
-
-      const nav_items_child = current_target.querySelector(".nav-items");
       const arrow_icon = current_target.querySelector(".v-icon");
-
-      const nav_items_child_height = nav_items_child.clientHeight;
-      const current_target_height = current_target.clientHeight;
-
-      current_target.style.height =
-        current_target_height + nav_items_child_height + "px";
+      const child_nav_expansion_activable = current_target.querySelector(
+        ".nav-expansion-activable"
+      );
 
       arrow_icon.classList.toggle("nav-icon-rotate-icon");
       current_target.classList.toggle("nav-item-collapse");
+      child_nav_expansion_activable.classList.add("nav-item-active");
+      child_nav_expansion_activable.classList.remove("nav-item-hover");
+
+      return;
     },
   },
 };
@@ -233,15 +302,28 @@ export default {
 .nav-item {
   overflow: hidden;
   transition: all 0.3s !important;
+  height: auto;
+}
+.nav-item-hover:hover {
+  background: var(--color-nav-item-hover);
 }
 .nav-item-collapse {
-  height: 24px !important;
+  height: 40px !important;
 }
 .nav-items {
   width: 100%;
 }
 .nav-icon-rotate-icon {
   transform: rotate(90deg);
+}
+.nav-item-active {
+  background: var(--color-nav-item-active);
+}
+.nav-expansion-activable {
+  border-radius: 4px;
+}
+.nav-non-expandable-active {
+  background: red;
 }
 .nav-item-header {
 }
