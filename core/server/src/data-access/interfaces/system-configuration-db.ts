593a5ec4aab59fd57ca9c2d483c3ebab2048b73b
huytran17 <alpha.lloyd1368@gmail.com>
2022-08-11 22:21:30 +0700

    system configuration data interface

---
+++ core/server/src/data-access/interfaces/system-configuration-db.ts
@@ -0,0 +1,8 @@
+import SystemConfiguration from "../../database/entities/system-configuration";
+import ISystemConfiguration from "../../database/interfaces/system-configuration";
+export default interface ISystemConfigurationDb {
+  findById: ({ _id }: { _id: string }) => Promise<SystemConfiguration | null>;
+  update: (
+    updatePayload: Partial<ISystemConfiguration>
+  ) => Promise<SystemConfiguration | null>;
+}
