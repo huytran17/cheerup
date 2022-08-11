062397fda44b6fe18de7976afd587b6dd49aea76
huytran17 <alpha.lloyd1368@gmail.com>
2022-08-11 21:19:41 +0700

    feedback data interface

---
+++ core/server/src/data-access/interfaces/feedback-db.ts
@@ -0,0 +1,32 @@
+import Feedback from "../../database/entities/feedback";
+import IFeedback from "../../database/interfaces/feedback";
+export default interface IFeedbackDb {
+  findAll: () => Promise<Feedback[] | null>;
+  findAllPaginated: ({
+    query,
+    page,
+    entries_per_page,
+  }: {
+    query: string;
+    page: number;
+    entries_per_page?: number;
+  }) => Promise<PaginatedFeedbackResult | null>;
+  findOne: () => Promise<Feedback | null>;
+  findById: ({ _id }: { _id: string }) => Promise<Feedback | null>;
+  insert: (payload: Partial<IFeedback>) => Promise<Feedback | null>;
+  delete: ({ _id }: { _id: string }) => Promise<Feedback | null>;
+  hardDelete: ({ _id }: { _id: string }) => Promise<Feedback | null>;
+  update: (updatePayload: Partial<IFeedback>) => Promise<Feedback | null>;
+}
+
+export interface PaginatedFeedbackResult {
+  data: Feedback[];
+  pagination: {
+    current_page: number | null;
+    from: number | null;
+    to: number | null;
+    per_page: number | null;
+    total: number | null;
+    total_pages: number | null;
+  };
+}
