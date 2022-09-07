7628cbe33dab931959bad4f9dff3fe6405f2cb72
huytran17 <alpha.lloyd1368@gmail.com>
2022-09-07 17:23:45 +0700

    app bar

--- core/user-dashboard/components/BaseAppBar.vue
+++ core/user-dashboard/components/BaseAppBar.vue
@@ -1,29 +1,48 @@
 <template>
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <!-- Brand/logo -->
-    <a class="navbar-brand" href="#">
-      <img src="bird.jpg" alt="logo" style="width: 40px" />
-    </a>
-
-    <!-- Links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link 3</a>
-      </li>
-    </ul>
-  </nav>
+  <v-app-bar elevation="0" color="white">
+    <div class="d-flex flex-column flex-sm-row justify-space-between w-100">
+      <div class="d-flex flex-row flex-sm-column justify-center pt-10 pt-sm-0">
+        <v-img
+          :src="logo_url"
+          :lazy-src="logo_url"
+          alt="logo"
+          max-width="100px"
+          contain
+        ></v-img>
+      </div>
+      <div class="d-flex flex-column justify-center pt-4 pt-sm-0">
+        <v-text-field
+          :placeholder="$t('Search post by title...')"
+          filled
+          rounded
+          dense
+          hide-details
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </div>
+    </div>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-    name: "BaseAppBar"
+  name: "BaseAppBar",
+  data() {
+    return {
+      logo_url: require("@/assets/images/app/small-logo.jpg"),
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+:deep(.v-toolbar__content) {
+  padding: 4px 0 !important;
+}
+:deep(.v-input__slot) {
+  padding-right: 18px !important;
+}
+:deep(.mdi-magnify) {
+  cursor: pointer;
+}
+</style>
