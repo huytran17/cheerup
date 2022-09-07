57d0829d9e6bbc597d998ad31e85d968dc8d11d6
huytran17 <alpha.lloyd1368@gmail.com>
2022-09-07 20:28:41 +0700

    update post mixin

--- core/user-dashboard/mixins/post.js
+++ core/user-dashboard/mixins/post.js
@@ -1,47 +1,19 @@
 import _ from "lodash";
-import { mapActions, mapMutations, mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  data() {
-    return {
-      titleRules: [(v) => !!v || this.$t("Title is required.")],
-    };
-  },
   computed: {
     ...mapGetters({
       post: "post/post",
       posts: "post/posts",
-      post_analys_data: "post/post_analys_data",
+      latest_posts: "post/latest_posts",
     }),
   },
   methods: {
     ...mapActions({
       GET_POSTS: "post/GET_POSTS",
       GET_POST: "post/GET_POST",
-      CREATE_POST: "post/CREATE_POST",
-      UPDATE_POST: "post/UPDATE_POST",
-      DELETE_POST: "post/DELETE_POST",
-      HARD_DELETE_POST: "post/HARD_DELETE_POST",
-      UNBLOCK_POST_COMMENT: "post/UNBLOCK_POST_COMMENT",
-      BLOCK_POST_COMMENT: "post/BLOCK_POST_COMMENT",
-      RESTORE_POST: "post/RESTORE_POST",
-      PUBLISH_POST: "post/PUBLISH_POST",
-      UNPUBLISH_POST: "post/UNPUBLISH_POST",
-      GET_POST_ANALYTICS: "post/GET_POST_ANALYTICS",
-      HIGHLIGHT_POST: "post/HIGHLIGHT_POST",
-      UNHIGHLIGHT_POST: "post/UNHIGHLIGHT_POST",
+      GET_LATEST_POSTS: "post/GET_LATEST_POSTS",
     }),
-    ...mapMutations({
-      SET_POST: "post/SET_POST",
-      SET_POSTS: "post/SET_POSTS",
-      UPDATE_POST_DATA: "post/UPDATE_POST_DATA",
-    }),
-
-    updatePostObject({ variable_path, data }) {
-      this.UPDATE_POST_DATA({
-        variable_path,
-        data,
-      });
-    },
   },
 };
