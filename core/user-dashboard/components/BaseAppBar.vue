70d5d147713fa29908388c79b1d5d0de8a4bf0f4
huytran17 <alpha.lloyd1368@gmail.com>
2022-09-07 16:19:39 +0700

    test new app bar

---
+++ core/user-dashboard/components/BaseAppBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="#">
+      <img src="bird.jpg" alt="logo" style="width: 40px" />
+    </a>
+
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 3</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "BaseAppBar"
+};
+</script>
+
+<style></style>
