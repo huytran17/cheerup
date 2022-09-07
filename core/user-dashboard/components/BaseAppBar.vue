3cb274c506f1b54cda749b370c649f2b0a2c2173
huytran17 <alpha.lloyd1368@gmail.com>
2022-09-07 20:14:45 +0700

    app bar

--- core/user-dashboard/components/BaseAppBar.vue
+++ core/user-dashboard/components/BaseAppBar.vue
@@ -2,32 +2,45 @@
   <div>
     <v-app-bar elevation="0" color="white">
       <div class="d-flex justify-center w-100">
-        <v-icon small color="black" class="clickable mr-2">mdi-facebook</v-icon>
-        <v-icon small color="black" class="clickable mr-2">mdi-instagram</v-icon>
-        <v-icon small color="black" class="clickable">mdi-twitter</v-icon>
+        <v-icon small color="black" class="clickable icon__font--medium mr-2"
+          >mdi-facebook</v-icon
+        >
+        <v-icon small color="black" class="clickable icon__font--medium mr-2"
+          >mdi-instagram</v-icon
+        >
+        <v-icon small color="black" class="clickable icon__font--medium"
+          >mdi-twitter</v-icon
+        >
       </div>
     </v-app-bar>
 
-    <div class="d-flex flex-column  justify-space-between w-100">
-      <div class="d-flex flex-row flex-sm-column justify-center pt-10 pt-sm-0">
+    <div class="d-flex flex-column justify-space-between w-100 pt-2">
+      <div class="d-flex flex-column pb-4">
         <v-img
           :src="logo_url"
           :lazy-src="logo_url"
           alt="logo"
-          max-width="100px"
+          max-width="140px"
           contain
+          class="mx-auto"
         ></v-img>
+        <div class="text-caption text-uppercase text-center pt-2 grey--text">
+          <span class="app-body" v-html="$t('App Name')"></span>
+        </div>
       </div>
-      <div class="d-flex flex-column justify-center pt-4 pt-sm-0">
-        <v-text-field
-          :placeholder="$t('Search post by title...')"
-          filled
-          rounded
-          dense
-          hide-details
-          append-icon="mdi-magnify"
-          :max-width="200"
-        ></v-text-field>
+      <div class="d-flex flex-column w-100">
+        <div class="mx-auto">
+          <v-text-field
+            id="search_box"
+            :placeholder="$t('Search post by title...')"
+            filled
+            rounded
+            dense
+            hide-details
+            append-icon="mdi-magnify"
+            class="w-100"
+          ></v-text-field>
+        </div>
       </div>
     </div>
   </div>
@@ -48,9 +61,7 @@ export default {
 :deep(.v-toolbar__content) {
   padding: 4px 0 !important;
 }
-:deep(.v-input__slot) {
-  padding-right: 18px !important;
-}
+
 :deep(.mdi-magnify) {
   cursor: pointer;
 }
