5eb97b4f826ebcad3dd6947b6b5366236856266c
huytran17 <alpha.lloyd1368@gmail.com>
2022-10-15 15:00:11 +0700

    update fetch user hook

--- core/user-dashboard/pages/about/index.vue
+++ core/user-dashboard/pages/about/index.vue
@@ -6,6 +6,16 @@
 import AboutMe from "@/components/about/AboutMe.vue";
 export default {
   name: "AboutMeIndex",
+  async asyncData({ store }) {
+    try {
+      const access_token = localStorage.getItem("access_token");
+      if (!_.isNil(access_token)) {
+        await store.dispatch("auth/GET_ME");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
   components: {
     AboutMe,
   },
--- core/user-dashboard/pages/profile/index.vue
+++ core/user-dashboard/pages/profile/index.vue
@@ -9,6 +9,16 @@ import BaseUpdateProfile from "@/components/user/BaseUpdateProfile";
 import BaseNoData from "@/components/BaseNoData";
 export default {
   name: "ProfileIndexPage",
+  async asyncData({ store }) {
+    try {
+      const access_token = localStorage.getItem("access_token");
+      if (!_.isNil(access_token)) {
+        await store.dispatch("auth/GET_ME");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
   components: {
     BaseUpdateProfile,
     BaseNoData,
