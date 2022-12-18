f728dd46c2c158c7351dedd8ce2bc3f75ba06dd1
huytran17 <alpha.lloyd1368@gmail.com>
2022-12-18 22:11:26 +0700

    wrap authenticated middleware into try/catch

--- core/user-dashboard/middleware/authenticated.ts
+++ core/user-dashboard/middleware/authenticated.ts
@@ -2,7 +2,11 @@ import { Context } from "@nuxt/types";
 import { isNil, isEmpty } from "lodash";
 
 export default function ({ store, redirect }: Context) {
-  const user = store.getters["auth/me"];
-  const hasUser = !isNil(user) && !isEmpty(user);
-  return hasUser || redirect("/login");
+  try {
+    const user = store.getters["auth/me"];
+    const hasUser = !isNil(user) && !isEmpty(user);
+    return hasUser || redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
 }
