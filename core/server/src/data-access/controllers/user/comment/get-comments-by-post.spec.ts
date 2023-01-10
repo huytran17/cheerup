443756c9a829b8588d0e00dcb7862a908ec97b9d
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-10 22:00:26 +0700

    unit test: get comments by post

---
+++ core/server/src/data-access/controllers/user/comment/get-comments-by-post.spec.ts
@@ -0,0 +1,96 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import {
+  fakeComment,
+  fakePost,
+  fakeQueryParams,
+} from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeCommentDb from "../../../make-comment-db";
+import makePostDb from "../../../make-post-db";
+import { CommentModel, PostModel } from "../../../models";
+import makeGetPost from "../../../../use-cases/post/get-post";
+import makeCreatePost from "../../../../use-cases/post/create-post";
+import makeCreateComment from "../../../../use-cases/comment/create-comment";
+import makeGetCommentsByPostPaginated from "../../../../use-cases/comment/get-comments-by-post-paginated";
+import makeGetCommentsByPostPaginatedController from "./get-comments-by-post-paginated";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+
+describe("getCommentsByPostPaginated", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("it should return a body that contains pagination data type of comments", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const commentDb = makeCommentDb({
+      commentDbModel: CommentModel,
+      moment,
+    });
+    const postDb = makePostDb({
+      postDbModel: PostModel,
+      moment,
+    });
+
+    const getPost = makeGetPost({ postDb, logger });
+    const createPost = makeCreatePost({ postDb, logger });
+    const createComment = makeCreateComment({ commentDb, logger });
+    const getCommentsByPostPaginated = makeGetCommentsByPostPaginated({
+      commentDb,
+      logger,
+    });
+
+    const mock_comment_data = fakeComment();
+    const mock_post_data = fakePost();
+
+    const created_post = await createPost({
+      postDetails: mock_post_data,
+    });
+
+    await createComment({
+      commentDetails: Object.assign(mock_comment_data, {
+        post: created_post._id,
+      }),
+    });
+
+    const getCommentsByPostPaginatedController =
+      makeGetCommentsByPostPaginatedController({
+        getCommentsByPostPaginated,
+        getPost,
+        logger,
+      });
+
+    const queryParams = fakeQueryParams();
+
+    const request = {
+      context: {
+        validated: {
+          ...queryParams,
+          post_id: created_post._id,
+        },
+      },
+    };
+
+    const result = await getCommentsByPostPaginatedController(request as any);
+
+    const expected = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: {
+        ...result,
+      },
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
