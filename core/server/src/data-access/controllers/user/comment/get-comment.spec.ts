9395a217d550c704d49955a6c3243756d981ee6b
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-10 21:36:57 +0700

    unit test: get comment

---
+++ core/server/src/data-access/controllers/user/comment/get-comment.spec.ts
@@ -0,0 +1,68 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { fakeComment } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeCommentDb from "../../../make-comment-db";
+import { CommentModel } from "../../../models";
+import makeCreateComment from "../../../../use-cases/comment/create-comment";
+import makeGetComment from "../../../../use-cases/comment/get-comment";
+import makeGetCommentController from "./get-comment";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+
+describe("getComment", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("it should return a body that contains a comment entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const commentDb = makeCommentDb({
+      commentDbModel: CommentModel,
+      moment,
+    });
+
+    const createComment = makeCreateComment({ commentDb, logger });
+    const getComment = makeGetComment({ commentDb, logger });
+
+    const mock_comment_data = fakeComment();
+
+    const created_comment = await createComment({
+      commentDetails: mock_comment_data,
+    });
+
+    const getCommentController = makeGetCommentController({
+      getComment,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: {
+          comment_id: created_comment._id,
+        },
+      },
+    };
+
+    const result = await getCommentController(request as any);
+
+    const expected = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: {
+        data: result?.body?.data,
+      },
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
