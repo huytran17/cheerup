d44eb5f9277b121aa431d59b575b99119146f6ed
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-10 20:18:58 +0700

    update: fake user data

--- core/server/__tests__/__mock__/make-fake-user.ts
+++ core/server/__tests__/__mock__/make-fake-user.ts
@@ -10,7 +10,7 @@ export default function makeFakeUser({ faker }: { faker: any }): FakeUser {
       hash_password:
         "$2b$10$NFtC.yQZ.JMI3HT1KBq8OOyY3EPu8oqmiXMQpGodmbYl9xblDX1gi",
       full_name: faker.name.fullName(),
-      is_blocked_comment: faker.datatype.boolean(),
+      is_blocked_comment: false,
       email: faker.internet.email(),
       created_at: faker.date.recent(),
       updated_at: faker.date.recent(),
