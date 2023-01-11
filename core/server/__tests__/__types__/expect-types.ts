70efc01cbedf02b6517ae49d2c9ad076c7da8792
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-11 21:49:03 +0700

    expect paginated result

--- core/server/__tests__/__types__/expect-types.ts
+++ core/server/__tests__/__types__/expect-types.ts
@@ -5,3 +5,19 @@ export type ExpectSingleEntity<T> = {
     data: T;
   };
 };
+
+export type ExpectPaginatedEntities<T> = {
+  headers: Record<string, unknown>;
+  statusCode: number;
+  body: {
+    data: T[];
+    pagination: {
+      current_page: number | null;
+      from: number | null;
+      to: number | null;
+      per_page: number | null;
+      total: number | null;
+      total_pages: number | null;
+    };
+  };
+};
