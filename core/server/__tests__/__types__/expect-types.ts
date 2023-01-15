c599ed98c1ba33c72970062f0174cdefa2c040a4
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-15 18:12:26 +0700

    unit test: add ExpectPaginatedPartialResult types

--- core/server/__tests__/__types__/expect-types.ts
+++ core/server/__tests__/__types__/expect-types.ts
@@ -29,3 +29,19 @@ export type ExpectPaginatedResult<T> = {
     };
   };
 };
+
+export type ExpectPaginatedPartialResult<T> = {
+  headers: Record<string, unknown>;
+  statusCode: number;
+  body: {
+    data: T[] & Partial<T>;
+    pagination: {
+      current_page: number | null;
+      from: number | null;
+      to: number | null;
+      per_page: number | null;
+      total: number | null;
+      total_pages: number | null;
+    };
+  };
+};
