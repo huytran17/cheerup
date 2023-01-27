4c8812383775e8bcb52120ba04f8781957600352
huytran17 <alpha.lloyd1368@gmail.com>
2023-01-27 15:06:06 +0700

    update: type validate for admin  and comment like type

--- core/server/src/data-access/controllers/admin/admin/validators/create-admin.ts
+++ core/server/src/data-access/controllers/admin/admin/validators/create-admin.ts
@@ -1,7 +1,7 @@
 const createAdminRules = {
   email: "required|email",
   full_name: "required|string",
-  type: "required|string",
+  type: "required|in:owner,collaborator,editor",
   password: "required|string|confirmed",
   password_confirmation: "required|string",
   is_auto_censorship_post: "boolean",
--- core/server/src/data-access/controllers/user/comment-like/validators/create-or-update-comment-like.ts
+++ core/server/src/data-access/controllers/user/comment-like/validators/create-or-update-comment-like.ts
@@ -1,6 +1,7 @@
 const createOrUpdateCommentLikeRules = {
   user: ["required", "regex:/^[0-9a-fA-F]{24}$/i"],
   comment: ["required", "regex:/^[0-9a-fA-F]{24}$/i"],
+  type: ["required", "string", "in:like,dislike"],
 };
 
 export default createOrUpdateCommentLikeRules;
