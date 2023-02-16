7412f4a687ac53b212b2712cc5d0cc5506724b8b
huytran17 <alpha.lloyd1368@gmail.com>
2023-02-16 21:36:17 +0700

    [Draft] post statistic UI

---
+++ core/admin-dashboard/components/dashboard/BaseStatisticCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <BaseCardWrapper>
+    <div class="d-flex flex-column card__statistic">
+      <div class="card__header">
+        <slot name="header"></slot>
+      </div>
+      <div class="card__description">
+        <slot name="description"></slot>
+      </div>
+      <div class="card__content">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </BaseCardWrapper>
+</template>
+
+<script>
+import BaseCardWrapper from "@/components/dashboard/BaseCardWrapper";
+
+export default {
+  name: "BaseStatisticCard",
+  components: {
+    BaseCardWrapper,
+  },
+};
+</script>
---
+++ core/admin-dashboard/components/dashboard/BaseStatisticPost.vue
@@ -0,0 +1,20 @@
+<template>
+  <BaseStatisticCard>
+    <template slot="header">
+      <h1>test</h1>
+    </template>
+  </BaseStatisticCard>
+</template>
+
+<script>
+import BaseStatisticCard from "@/components/dashboard/BaseStatisticCard";
+
+export default {
+  name: "BaseStatisticPost",
+  components: {
+    BaseStatisticCard,
+  },
+};
+</script>
+
+<style></style>
