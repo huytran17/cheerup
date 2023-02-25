ce3a180436967b40e4c9d21d050a402d9e2de191
huytran17 <alpha.lloyd1368@gmail.com>
2023-02-25 16:07:17 +0700

    rmv unused codes

--- core/admin-dashboard/constants/index.ts
+++ core/admin-dashboard/constants/index.ts
@@ -1,17 +1,3 @@
-const S3_UPLOAD_URL_TYPES = {
-  GALLERY_ITEM: `${process.env.SERVER_URL}/admin/gallery/upload-gallery-item`,
-  CATEGORY_THUMBNAIL: `${process.env.SERVER_URL}/admin/category/upload-thumbnail`,
-  POST_THUMBNAIL: `${process.env.SERVER_URL}/admin/post/upload-thumbnail`,
-  ADMIN_AVATAR: `${process.env.SERVER_URL}/admin/admin/upload-avatar`,
-  USER_AVATAR: `${process.env.SERVER_URL}/admin/user/upload-avatar`,
-  SYSTEM_CONFIG_ADMIN_META_LOGO: `${process.env.SERVER_URL}/admin/system-configuration/upload-admin-meta-logo`,
-  SYSTEM_CONFIG_CLIENT_META_LOGO: `${process.env.SERVER_URL}/admin/system-configuration/upload-client-meta-logo`,
-  SYSTEM_CONFIG_ADMIN_META_FAVICON: `${process.env.SERVER_URL}/admin/system-configuration/upload-admin-meta-favicon`,
-  SYSTEM_CONFIG_ADMIN_META_FOLDER_ICON: `${process.env.SERVER_URL}/admin/system-configuration/upload-admin-meta-folder-icon`,
-  SYSTEM_CONFIG_CLIENT_META_FAVICON: `${process.env.SERVER_URL}/admin/system-configuration/upload-client-meta-favicon`,
-  SYSTEM_CONFIG_CLIENT_META_OWNER_AVATAR: `${process.env.SERVER_URL}/admin/system-configuration/upload-client-meta-owner-avatar`,
-};
-
 const ADMIN_TYPES = {
   OWNER: "owner",
   COLLABORATOR: "collaborator",
@@ -30,9 +16,8 @@ const HTTP_STATUS_CODE = {
 };
 
 export default Object.freeze({
-  S3_UPLOAD_URL_TYPES,
   ADMIN_TYPES,
   HTTP_STATUS_CODE,
 });
 
-export { S3_UPLOAD_URL_TYPES, ADMIN_TYPES, HTTP_STATUS_CODE };
+export { ADMIN_TYPES, HTTP_STATUS_CODE };
