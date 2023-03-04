a8e05d9191cbaac176fc5abdf6f0948747f7b42e
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-04 18:02:25 +0700

    add 403 page

---
+++ core/admin-dashboard/pages/403/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-fle flex-column text-center mt-2">
+    <div class="text-h5 error--text">
+      <span class="app-title" v-html="$t('403')"></span>
+    </div>
+    <div class="text-h6 text-uppercase">
+      <span class="app-title" v-html="$t('Forbidden!')"></span>
+    </div>
+    <div class="text-h6 text-uppercase">
+      <span
+        class="app-title"
+        v-html="$t('You are not allowed to enter here.')"
+      ></span>
+    </div>
+    <div>
+      <v-img :src="image" :alt="$t('403')" contain :height="400"></v-img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "403",
+  data() {
+    return {
+      image: require("@/assets/images/app/wonder-guy.png"),
+    };
+  },
+};
+</script>
+
+<style></style>
