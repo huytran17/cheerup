294fba804d91189690530423e40e7a5e89bc5e60
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-04 18:07:39 +0700

    add 400 page

---
+++ core/admin-dashboard/pages/400/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-fle flex-column text-center mt-2">
+    <div class="text-h5 error--text">
+      <span class="app-title" v-html="$t('400')"></span>
+    </div>
+    <div class="text-h6 text-uppercase">
+      <span class="app-title" v-html="$t('Bad Request!')"></span>
+    </div>
+    <div class="text-h6 text-uppercase">
+      <span
+        class="app-title"
+        v-html="$t('Can not process invalid data.')"
+      ></span>
+    </div>
+    <div>
+      <v-img :src="image" :alt="$t('403')" contain :height="400"></v-img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "403",
+  data() {
+    return {
+      image: require("@/assets/images/app/thinking-man.png"),
+    };
+  },
+};
+</script>
+
+<style></style>
