b2ca85b247a84fe7db395977e960eb04ccb07114
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-04 18:22:43 +0700

    update error page

--- core/admin-dashboard/layouts/error.vue
+++ core/admin-dashboard/layouts/error.vue
@@ -1,41 +1,57 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div v-if="error.statusCode === 404">
+    <div class="d-fle flex-column text-center mt-2">
+      <div class="text-h5 error--text">
+        <span class="app-title" v-html="$t('404')"></span>
+      </div>
+      <div class="text-h6 text-uppercase">
+        <span class="app-title" v-html="$t('Not found!')"></span>
+      </div>
+      <div class="text-h6 text-uppercase">
+        <span
+          class="app-title"
+          v-html="
+            $t(
+              'The page you are looking for might be removed or temporary unavailable.'
+            )
+          "
+        ></span>
+      </div>
+      <div>
+        <v-img :src="image" :alt="$t('404')" contain :height="400"></v-img>
+      </div>
+    </div>
+  </div>
+  <h1 v-else>
+    {{ otherError }}
+  </h1>
 </template>
 
 <script>
 export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
+  name: "EmptyLayout",
+  layout: "empty",
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      pageNotFound: "404 Not Found",
+      otherError: "An error occurred",
+      image: require("@/assets/images/app/student-girl.png"),
+    };
   },
-  head () {
+  head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title
-    }
-  }
-}
+      title,
+    };
+  },
+};
 </script>
 
 <style scoped>
