d171e4e90aedbb32da9b5a78ec35dfc768e5c1d4
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-04 14:30:15 +0700

    cleanup: rmv unused codes

--- core/user-dashboard/middleware/authentication.ts
+++ core/user-dashboard/middleware/authentication.ts
@@ -2,7 +2,7 @@ import { Context } from "@nuxt/types";
 
 export default async function ({ store }: Context) {
   try {
-    const access_token = localStorage.getItem("access_token");
+    const access_token = localStorage.getItem("admin_access_token");
 
     const { email } = await store.dispatch("auth/VERIFY_ACCESS", {
       access_token,
