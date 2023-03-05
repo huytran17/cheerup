4d015f287d400a32736e684f6332d735a05ef8eb
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-05 19:12:15 +0700

    admin dashboard: add language switcher

--- core/admin-dashboard/components/dashboard/BaseAnalysisToggler.vue
+++ core/admin-dashboard/components/dashboard/BaseAnalysisToggler.vue
@@ -1,57 +1,59 @@
 <template>
-  <div class="d-flex mx-3">
-    <div class="mr-2 d-flex flex-column justify-center">
+  <div class="d-flex flex-column">
+    <div class="mr-2 d-flex flex-column justify-center mr-auto ml-3 mb-3">
       <BaseLanguageSwitcher />
     </div>
-    <div
-      class="text-body-1 d-flex flex-column justify-center pr-4 primary--text"
-    >
-      <span class="app-body" v-html="$t('Statistical Cycle')"></span>
-    </div>
-    <v-menu
-      :close-on-content-click="false"
-      transition="scale-transition"
-      offset-y
-      min-width="auto"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-text-field
+    <div class="d-flex mx-3">
+      <div
+        class="text-body-1 d-flex flex-column justify-center pr-4 primary--text"
+      >
+        <span class="app-body" v-html="$t('Statistical Cycle')"></span>
+      </div>
+      <v-menu
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date_range"
+            v-bind="attrs"
+            v-on="on"
+            readonly
+            hide-details
+            clearable
+            class="pt-0"
+            :label="$t('Pick a range')"
+          ></v-text-field>
+        </template>
+        <v-date-picker
           v-model="date_range"
-          v-bind="attrs"
-          v-on="on"
-          readonly
+          :label="$t('Period')"
+          flat
+          range
           hide-details
-          clearable
-          class="pt-0"
-          :label="$t('Pick a range')"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        v-model="date_range"
-        :label="$t('Period')"
-        flat
-        range
-        hide-details
-      ></v-date-picker>
-    </v-menu>
-    <v-btn-toggle v-model="unit" color="primary" dense group mandatory>
-      <v-btn v-for="(button, index) in buttons" :key="index">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <div v-bind="attrs" v-on="on" class="text-body-2">
-              <span class="app-title" v-html="$t(button.text)"></span>
+        ></v-date-picker>
+      </v-menu>
+      <v-btn-toggle v-model="unit" color="primary" dense group mandatory>
+        <v-btn v-for="(button, index) in buttons" :key="index">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <div v-bind="attrs" v-on="on" class="text-body-2">
+                <span class="app-title" v-html="$t(button.text)"></span>
+              </div>
+            </template>
+            <div class="text-body-2">
+              <span class="app-body" v-html="$t(button.full_text)"></span>
             </div>
-          </template>
-          <div class="text-body-2">
-            <span class="app-body" v-html="$t(button.full_text)"></span>
-          </div>
-        </v-tooltip>
-      </v-btn>
-    </v-btn-toggle>
-    <div class="d-flex flex-column justify-center">
-      <v-btn color="primary" tile depressed @click="refreshAnalysisData">
-        <v-icon>mdi-google-analytics</v-icon>
-      </v-btn>
+          </v-tooltip>
+        </v-btn>
+      </v-btn-toggle>
+      <div class="d-flex flex-column justify-center">
+        <v-btn color="primary" tile depressed @click="refreshAnalysisData">
+          <v-icon>mdi-google-analytics</v-icon>
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
