9862b2d55d067c8be33ff47728834f118092c06b
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-11 18:49:33 +0700

    unit test admin dashboard: update admin

---
+++ core/server/src/data-access/controllers/admin/admin/update-admin.spec.ts
@@ -0,0 +1,72 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeAdmin } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeAdminDb from "../../../make-admin-db";
+import { AdminModel } from "../../../models";
+import makeGetAdmin from "../../../../use-cases/admin/get-admin";
+import makeCreateAdmin from "../../../../use-cases/admin/create-admin";
+import makeUpdateAdmin from "../../../../use-cases/admin/update-admin";
+import makeUpdateAdminController from "./update-admin";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Admin from "../../../../database/entities/admin";
+
+describe("updateAdmin", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an admin entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const adminDb = makeAdminDb({
+      adminDbModel: AdminModel,
+      moment,
+    });
+
+    const createAdmin = makeCreateAdmin({ adminDb, logger });
+    const updateAdmin = makeUpdateAdmin({ adminDb, logger });
+    const getAdmin = makeGetAdmin({
+      adminDb,
+      logger,
+    });
+
+    const mock_admin_data = fakeAdmin();
+
+    const created_admin = await createAdmin({
+      adminDetails: mock_admin_data,
+    });
+
+    const updateAdminController = makeUpdateAdminController({
+      getAdmin,
+      updateAdmin,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: created_admin,
+      },
+    };
+
+    const result = await updateAdminController(request as any);
+
+    const expected: ExpectSingleResult<Admin> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
