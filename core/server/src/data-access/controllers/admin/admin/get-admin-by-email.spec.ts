732f54fca4467235f0df7166c5355c43822ee807
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-11 18:22:26 +0700

    unit test admin dashboard: get admin by email

---
+++ core/server/src/data-access/controllers/admin/admin/get-admin-by-email.spec.ts
@@ -0,0 +1,68 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeAdmin } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeAdminDb from "../../../make-admin-db";
+import { AdminModel } from "../../../models";
+import makeGetAdminByEmail from "../../../../use-cases/admin/get-admin-by-email";
+import makeCreateAdmin from "../../../../use-cases/admin/create-admin";
+import makeAdminByEmailController from "./get-admin-by-email";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Admin from "../../../../database/entities/admin";
+
+describe("getAdminByEmail", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an admin entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const adminDb = makeAdminDb({
+      adminDbModel: AdminModel,
+      moment,
+    });
+
+    const createAdmin = makeCreateAdmin({ adminDb, logger });
+    const getAdminByEmail = makeGetAdminByEmail({
+      adminDb,
+      logger,
+    });
+
+    const mock_admin_data = fakeAdmin();
+
+    const created_admin = await createAdmin({
+      adminDetails: mock_admin_data,
+    });
+
+    const getAdminByEmailController = makeAdminByEmailController({
+      getAdminByEmail,
+    });
+
+    const request = {
+      context: {
+        validated: created_admin,
+      },
+    };
+
+    const result = await getAdminByEmailController(request as any);
+
+    const expected: ExpectSingleResult<Admin> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
