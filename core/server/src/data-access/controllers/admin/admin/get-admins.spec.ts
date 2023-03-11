53cdbb20fbc46586ae8c80979fe2d3862f7393c0
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-11 18:33:22 +0700

    unit test admin dashboard: get admins

---
+++ core/server/src/data-access/controllers/admin/admin/get-admins.spec.ts
@@ -0,0 +1,68 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectMultipleResults } from "../../../../../__tests__/__types__/expect-types";
+import { fakeAdmin } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeAdminDb from "../../../make-admin-db";
+import { AdminModel } from "../../../models";
+import makeGetAdmins from "../../../../use-cases/admin/get-admins";
+import makeCreateAdmin from "../../../../use-cases/admin/create-admin";
+import makeAdminsController from "./get-admins";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Admin from "../../../../database/entities/admin";
+
+describe("getAdmins", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an array of admin entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const adminDb = makeAdminDb({
+      adminDbModel: AdminModel,
+      moment,
+    });
+
+    const createAdmin = makeCreateAdmin({ adminDb, logger });
+    const getAdmins = makeGetAdmins({
+      adminDb,
+      logger,
+    });
+
+    const mock_admin_data = fakeAdmin();
+
+    const created_admin = await createAdmin({
+      adminDetails: mock_admin_data,
+    });
+
+    const getAdminsController = makeAdminsController({
+      getAdmins,
+    });
+
+    const request = {
+      context: {
+        validated: created_admin,
+      },
+    };
+
+    const result = await getAdminsController(request as any);
+
+    const expected: ExpectMultipleResults<Admin> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
