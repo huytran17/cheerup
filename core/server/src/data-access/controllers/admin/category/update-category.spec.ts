fc146ce1a4ce0bc2b2e04591778fa4efb9212556
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-23 18:28:33 +0700

    unit test admin dashboard: update category

---
+++ core/server/src/data-access/controllers/admin/category/update-category.spec.ts
@@ -0,0 +1,72 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeCategory } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeCategoryDb from "../../../make-category-db";
+import { CategoryModel } from "../../../models";
+import makeCreateCategory from "../../../../use-cases/category/create-category";
+import makeGetCategory from "../../../../use-cases/category/get-category";
+import makeGetCategoryByTitle from "../../../../use-cases/category/get-category-by-title";
+import makeUpdateCategory from "../../../../use-cases/category/update-category";
+import makeUpdateCategoryController from "./update-category";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Category from "../../../../database/entities/category";
+
+describe("updateCategory", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an category entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const categoryDb = makeCategoryDb({
+      categoryDbModel: CategoryModel,
+      moment,
+    });
+
+    const createCategory = makeCreateCategory({ categoryDb, logger });
+    const getCategory = makeGetCategory({ categoryDb, logger });
+    const updateCategory = makeUpdateCategory({ categoryDb, logger });
+    const getCategoryByTitle = makeGetCategoryByTitle({ categoryDb, logger });
+
+    const mock_category_data = fakeCategory();
+
+    const updateCategoryController = makeUpdateCategoryController({
+      getCategory,
+      updateCategory,
+      getCategoryByTitle,
+      logger,
+    });
+
+    const created_category = await createCategory({
+      categoryDetails: mock_category_data,
+    });
+
+    const request = {
+      context: {
+        validated: created_category,
+      },
+    };
+
+    const result = await updateCategoryController(request as any);
+
+    const expected: ExpectSingleResult<Category> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
