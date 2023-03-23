06a525264fc78971e08b87429c303019e757bf3e
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-23 18:03:20 +0700

    unit test admin dashboard: delete category

---
+++ core/server/src/data-access/controllers/admin/category/delete-category.spec.ts
@@ -0,0 +1,69 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeCategory } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeCategoryDb from "../../../make-category-db";
+import { CategoryModel } from "../../../models";
+import makeCreateCategory from "../../../../use-cases/category/create-category";
+import makeDeleteCategory from "../../../../use-cases/category/delete-category";
+import makeGetCategory from "../../../../use-cases/category/get-category";
+import makeDeleteCategoryController from "./delete-category";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Category from "../../../../database/entities/category";
+
+describe("deleteCategory", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an category entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const categoryDb = makeCategoryDb({
+      categoryDbModel: CategoryModel,
+      moment,
+    });
+
+    const createCategory = makeCreateCategory({ categoryDb, logger });
+    const deleteCategory = makeDeleteCategory({ categoryDb, logger });
+    const getCategory = makeGetCategory({ categoryDb, logger });
+
+    const mock_category_data = fakeCategory();
+
+    const deleteCategoryController = makeDeleteCategoryController({
+      getCategory,
+      deleteCategory,
+      logger,
+    });
+
+    const created_category = await createCategory({
+      categoryDetails: mock_category_data,
+    });
+
+    const request = {
+      context: {
+        validated: created_category,
+      },
+    };
+
+    const result = await deleteCategoryController(request as any);
+
+    const expected: ExpectSingleResult<Category> = {
+      headers,
+      statusCode: HttpStatusCode.CREATED,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
