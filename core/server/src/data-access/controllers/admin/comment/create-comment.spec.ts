4ba59918ed7c4389665e72278431afb3c5e579a1
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-23 18:49:05 +0700

    unit test admin dashboard: crate comment

---
+++ core/server/src/data-access/controllers/admin/comment/create-comment.spec.ts
@@ -0,0 +1,60 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeComment } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeCommentDb from "../../../make-comment-db";
+import { CommentModel } from "../../../models";
+import makeCreateComment from "../../../../use-cases/comment/create-comment";
+import makeCreateCommentController from "./create-comment";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Comment from "../../../../database/entities/comment";
+
+describe("createComment", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an comment entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const commentDb = makeCommentDb({
+      commentDbModel: CommentModel,
+      moment,
+    });
+
+    const createComment = makeCreateComment({ commentDb, logger });
+
+    const mock_comment_data = fakeComment();
+
+    const createCommentController = makeCreateCommentController({
+      createComment,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: mock_comment_data,
+      },
+    };
+
+    const result = await createCommentController(request as any);
+
+    const expected: ExpectSingleResult<Comment> = {
+      headers,
+      statusCode: HttpStatusCode.CREATED,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
