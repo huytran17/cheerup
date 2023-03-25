eebeb82181df03109ed0ffeef96f31c01c71e04b
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 18:52:09 +0700

    unit test admin dashboard: getUsers

---
+++ core/server/src/data-access/controllers/admin/user/get-users.spec.ts
@@ -0,0 +1,57 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectMultipleResults } from "../../../../../__tests__/__types__/expect-types";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeUserDb from "../../../make-user-db";
+import { UserModel } from "../../../models";
+import makeGetUsers from "../../../../use-cases/user/get-users";
+import makeGetUsersController from "./get-users";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import User from "../../../../database/entities/user";
+
+describe("getUsers", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an user entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const userDb = makeUserDb({
+      userDbModel: UserModel,
+      moment,
+    });
+
+    const getUsers = makeGetUsers({ userDb, logger });
+
+    const getUsersController = makeGetUsersController({
+      getUsers,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: {},
+      },
+    };
+
+    const result = await getUsersController(request as any);
+
+    const expected: ExpectMultipleResults<User> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
