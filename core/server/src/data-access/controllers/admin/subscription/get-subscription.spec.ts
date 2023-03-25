e06b38f5122c3afe709290a6ef47926f9c10ea00
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 17:52:45 +0700

    unit test admin dashboard: getSubscription

---
+++ core/server/src/data-access/controllers/admin/subscription/get-subscription.spec.ts
@@ -0,0 +1,69 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeSubscription } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeSubscriptionDb from "../../../make-subscription-db";
+import { SubscriptionModel } from "../../../models";
+import makeCreateSubscription from "../../../../use-cases/subscription/create-subscription";
+import makeGetSubscription from "../../../../use-cases/subscription/get-subscription";
+import makeGetSubscriptionController from "./get-subscription";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Subscription from "../../../../database/entities/subscription";
+
+describe("getSubscription", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an subscription entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const subscriptionDb = makeSubscriptionDb({
+      subscriptionDbModel: SubscriptionModel,
+      moment,
+    });
+
+    const createSubscription = makeCreateSubscription({
+      subscriptionDb,
+      logger,
+    });
+    const getSubscription = makeGetSubscription({ subscriptionDb, logger });
+
+    const mock_subscription_data = fakeSubscription();
+
+    const created_subscription = await createSubscription({
+      subscriptionDetails: mock_subscription_data,
+    });
+
+    const getSubscriptionController = makeGetSubscriptionController({
+      getSubscription,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: { subscription_id: created_subscription._id },
+      },
+    };
+
+    const result = await getSubscriptionController(request as any);
+
+    const expected: ExpectSingleResult<Subscription> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
