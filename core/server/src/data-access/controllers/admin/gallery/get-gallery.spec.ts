e18dcb83f2cc50b7e108370b010c5ccf9f288ec1
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 16:09:17 +0700

    unit test admin dashboard: get gallery

---
+++ core/server/src/data-access/controllers/admin/gallery/get-gallery.spec.ts
@@ -0,0 +1,66 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeGallery } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeGalleryDb from "../../../make-gallery-db";
+import { GalleryModel } from "../../../models";
+import makeCreateGallery from "../../../../use-cases/gallery/create-gallery";
+import makeGetGallery from "../../../../use-cases/gallery/get-gallery";
+import makeGetGalleryController from "./get-gallery";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Gallery from "../../../../database/entities/gallery";
+
+describe("getGallery", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an gallery entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const galleryDb = makeGalleryDb({
+      galleryDbModel: GalleryModel,
+      moment,
+    });
+
+    const createGallery = makeCreateGallery({ galleryDb, logger });
+    const getGallery = makeGetGallery({ galleryDb, logger });
+
+    const mock_gallery_data = fakeGallery();
+
+    const created_gallery = await createGallery({
+      galleryDetails: mock_gallery_data,
+    });
+
+    const getGalleryController = makeGetGalleryController({
+      getGallery,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: created_gallery,
+      },
+    };
+
+    const result = await getGalleryController(request as any);
+
+    const expected: ExpectSingleResult<Gallery> = {
+      headers,
+      statusCode: HttpStatusCode.CREATED,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
