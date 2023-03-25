eb42e712220fd208cf66434506a4162432dcde2c
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 17:27:11 +0700

    unit test admin dashboard: hardDeletePost

---
+++ core/server/src/data-access/controllers/admin/post/hard-delete-post.spec.ts
@@ -0,0 +1,69 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakePost } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makePostDb from "../../../make-post-db";
+import { PostModel } from "../../../models";
+import makeCreatePost from "../../../../use-cases/post/create-post";
+import makeGetPost from "../../../../use-cases/post/get-post";
+import makeHardDeletePost from "../../../../use-cases/post/hard-delete-post";
+import makeHardDeletePostController from "./hard-delete-post";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Post from "../../../../database/entities/post";
+
+describe("hardDeletePost", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an post entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const postDb = makePostDb({
+      postDbModel: PostModel,
+      moment,
+    });
+
+    const createPost = makeCreatePost({ postDb, logger });
+    const getPost = makeGetPost({ postDb, logger });
+    const hardDeletePost = makeHardDeletePost({ postDb, logger });
+
+    const mock_post_data = fakePost();
+
+    const created_post = await createPost({
+      postDetails: mock_post_data,
+    });
+
+    const hardDeletePostController = makeHardDeletePostController({
+      getPost,
+      hardDeletePost,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: created_post,
+      },
+    };
+
+    const result = await hardDeletePostController(request as any);
+
+    const expected: ExpectSingleResult<Post> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
