01909d8cc001205a185da49ae19fcf44c92f2c8c
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 18:13:28 +0700

    unit test admin dashboard: getLatestSystemConfiguration

---
+++ core/server/src/data-access/controllers/admin/system-configuration/get-latest-system-configuration.spec.ts
@@ -0,0 +1,73 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeSystemConfiguration } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeSystemConfigurationDb from "../../../make-system-configuration-db";
+import { SystemConfigurationModel } from "../../../models";
+import makeCreateSystemConfiguration from "../../../../use-cases/system-configuration/create-system-configuration";
+import makeGetLatestSystemConfiguration from "../../../../use-cases/system-configuration/get-latest-system-configuration";
+import makeGetLatestSystemConfigurationController from "./get-latest-system-configuration";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import SystemConfiguration from "../../../../database/entities/system-configuration";
+
+describe("getLatestSystemConfiguration", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an system configuration entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const systemConfigurationDb = makeSystemConfigurationDb({
+      systemConfigurationDbModel: SystemConfigurationModel,
+      moment,
+    });
+
+    const createSystemConfiguration = makeCreateSystemConfiguration({
+      systemConfigurationDb,
+      logger,
+    });
+    const getLatestSystemConfiguration = makeGetLatestSystemConfiguration({
+      systemConfigurationDb,
+      logger,
+    });
+
+    const mock_system_configuration_data = fakeSystemConfiguration();
+
+    await createSystemConfiguration({
+      systemConfigurationDetails: mock_system_configuration_data,
+    });
+
+    const getLatestSystemConfigurationController =
+      makeGetLatestSystemConfigurationController({
+        getLatestSystemConfiguration,
+        logger,
+      });
+
+    const request = {
+      context: {
+        validated: {},
+      },
+    };
+
+    const result = await getLatestSystemConfigurationController(request as any);
+
+    const expected: ExpectSingleResult<SystemConfiguration> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
