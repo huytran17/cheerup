cea0c1275c69401941bd7ec289e1393dd1c57ba6
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 18:48:21 +0700

    unit test admin dashboard: getUserByEmail

---
+++ core/server/src/data-access/controllers/admin/user/get-user-by-email.spec.ts
@@ -0,0 +1,65 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeUser } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeUserDb from "../../../make-user-db";
+import { UserModel } from "../../../models";
+import makeCreateUser from "../../../../use-cases/user/create-user";
+import makeGetUserByEmail from "../../../../use-cases/user/get-user-by-email";
+import makeGetUserByEmailController from "./get-user-by-email";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import User from "../../../../database/entities/user";
+
+describe("getUserByEmail", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an user entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const userDb = makeUserDb({
+      userDbModel: UserModel,
+      moment,
+    });
+
+    const createUser = makeCreateUser({ userDb, logger });
+    const getUserByEmail = makeGetUserByEmail({ userDb, logger });
+
+    const mock_user_data = fakeUser();
+
+    const created_user = await createUser({
+      userDetails: mock_user_data,
+    });
+
+    const getUserByEmailController = makeGetUserByEmailController({
+      getUserByEmail,
+    });
+
+    const request = {
+      context: {
+        validated: created_user,
+      },
+    };
+
+    const result = await getUserByEmailController(request as any);
+
+    const expected: ExpectSingleResult<User> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
