094de43b9b0a4f80b9b1bf936d4ba83b670e425a
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-25 16:05:46 +0700

    unit test admin dashboard: get comments paginated

---
+++ core/server/src/data-access/controllers/admin/gallery/get-galleries-paginated.spec.ts
@@ -0,0 +1,73 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectMultipleResults } from "../../../../../__tests__/__types__/expect-types";
+import {
+  fakeGallery,
+  fakeQueryParams,
+} from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeGalleryDb from "../../../make-gallery-db";
+import { GalleryModel } from "../../../models";
+import makeCreateGallery from "../../../../use-cases/gallery/create-gallery";
+import makeGetGalleriesPaginated from "../../../../use-cases/gallery/get-galleries-paginated";
+import makeGetGalleriesPaginatedController from "./get-galleries-paginated";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import Gallery from "../../../../database/entities/gallery";
+
+describe("getGalleriesPaginated", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an array of gallery entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const galleryDb = makeGalleryDb({
+      galleryDbModel: GalleryModel,
+      moment,
+    });
+
+    const createGallery = makeCreateGallery({ galleryDb, logger });
+    const getGalleriesPaginated = makeGetGalleriesPaginated({
+      galleryDb,
+      logger,
+    });
+
+    const mock_gallery_data = fakeGallery();
+    const mock_query_params_data = fakeQueryParams();
+
+    await createGallery({ galleryDetails: mock_gallery_data });
+
+    const getGalleriesPaginatedController = makeGetGalleriesPaginatedController(
+      {
+        getGalleriesPaginated,
+        logger,
+      }
+    );
+
+    const request = {
+      context: {
+        validated: mock_query_params_data,
+      },
+    };
+
+    const result = await getGalleriesPaginatedController(request as any);
+
+    const expected: ExpectMultipleResults<Gallery> = {
+      headers,
+      statusCode: HttpStatusCode.CREATED,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
