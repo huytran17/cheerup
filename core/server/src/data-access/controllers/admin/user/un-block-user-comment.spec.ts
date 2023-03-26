6830fa69e5629dc7a03a3750cefc7a1b4bf843c2
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-26 14:50:23 +0700

    unit test admin dashboard: unBlockUserComment

---
+++ core/server/src/data-access/controllers/admin/user/un-block-user-comment.spec.ts
@@ -0,0 +1,69 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeUser } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeUserDb from "../../../make-user-db";
+import { UserModel } from "../../../models";
+import makeCreateUser from "../../../../use-cases/user/create-user";
+import makeUpdateUser from "../../../../use-cases/user/update-user";
+import makeGetUser from "../../../../use-cases/user/get-user";
+import makeUnBlockUserCommentController from "./un-block-user-comment";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import User from "../../../../database/entities/user";
+
+describe("unBlockUserComment", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an user entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const userDb = makeUserDb({
+      userDbModel: UserModel,
+      moment,
+    });
+
+    const createUser = makeCreateUser({ userDb, logger });
+    const getUser = makeGetUser({ userDb, logger });
+    const updateUser = makeUpdateUser({ userDb, logger });
+
+    const mock_user_data = fakeUser();
+
+    const created_user = await createUser({
+      userDetails: mock_user_data,
+    });
+
+    const unblockUserCommentController = makeUnBlockUserCommentController({
+      getUser,
+      updateUser,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: created_user,
+      },
+    };
+
+    const result = await unblockUserCommentController(request as any);
+
+    const expected: ExpectSingleResult<User> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
