ef91c1d5ccb03c4f524691b0599ebd70923726b9
huytran17 <alpha.lloyd1368@gmail.com>
2023-03-26 14:47:54 +0700

    unit test admin dashboard: hardDeleteUser

---
+++ core/server/src/data-access/controllers/admin/user/hard-delete-user.spec.ts
@@ -0,0 +1,69 @@
+import moment from "moment";
+import {
+  connectDatabase,
+  clearDatabase,
+} from "../../../../../__tests__/jest-mongo";
+import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
+import { fakeUser } from "../../../../../__tests__/__mock__";
+import { logger } from "../../../../../__tests__/jest-logger";
+import makeUserDb from "../../../make-user-db";
+import { UserModel } from "../../../models";
+import makeCreateUser from "../../../../use-cases/user/create-user";
+import makeHardDeleteUser from "../../../../use-cases/user/hard-delete-user";
+import makeGetUser from "../../../../use-cases/user/get-user";
+import makeHardDeleteUserController from "./hard-delete-user";
+import { HttpStatusCode } from "../../../../constants/http-status-code";
+import User from "../../../../database/entities/user";
+
+describe("hardDeleteUser", () => {
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+
+  afterAll(async () => {
+    await clearDatabase();
+  });
+
+  it("should return a body that contains an user entity", async () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const userDb = makeUserDb({
+      userDbModel: UserModel,
+      moment,
+    });
+
+    const createUser = makeCreateUser({ userDb, logger });
+    const getUser = makeGetUser({ userDb, logger });
+    const hardDeleteUser = makeHardDeleteUser({ userDb, logger });
+
+    const mock_user_data = fakeUser();
+
+    const created_user = await createUser({
+      userDetails: mock_user_data,
+    });
+
+    const hardDeleteUserController = makeHardDeleteUserController({
+      getUser,
+      hardDeleteUser,
+      logger,
+    });
+
+    const request = {
+      context: {
+        validated: created_user,
+      },
+    };
+
+    const result = await hardDeleteUserController(request as any);
+
+    const expected: ExpectSingleResult<User> = {
+      headers,
+      statusCode: HttpStatusCode.OK,
+      body: result?.body,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
