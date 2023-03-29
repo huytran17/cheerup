214e55532d61c5895c351a3a958c576a6ec98eff
huytran <huytv@mor.com.vn>
2023-03-29 14:14:18 +0700

    [Draft] passport facebook

--- core/server/src/config/passport/facebook/index.ts
+++ core/server/src/config/passport/facebook/index.ts
@@ -1,5 +1,8 @@
 import passport_fb, { StrategyOptionWithRequest } from "passport-facebook";
 import { PassportStatic } from "passport";
+import { UserDb } from "../../../data-access";
+import { hashPassword } from "../../password";
+import randomString from "randomstring";
 
 export default function initializeFacebook(
   passport: PassportStatic,
@@ -14,19 +17,45 @@ export default function initializeFacebook(
   const opts: StrategyOptionWithRequest = {
     ...options,
     authType: "reauthenticate",
-    scope: ["user_friends", "manage_pages"],
     profileFields: ["id", "displayName", "photos", "email"],
     passReqToCallback: true,
+    enableProof: true,
   };
 
   passport.use(
     "facebook",
     new FacebookStrategy(opts, async function (
+      req,
       accessToken,
       refreshToken,
       profile,
-      cb
-    ) {})
+      done
+    ) {
+      const exist = await UserDb.findByEmail({
+        email: profile.emails[0].value,
+        deleted_at: { $in: [null, undefined] },
+      });
+
+      if (exist) {
+        return done(null, exist);
+      }
+
+      const random_password = randomString.generate();
+      const hashed_password = await hashPassword({
+        password: random_password,
+        password_confirmation: random_password,
+      });
+
+      const userDetails = {
+        email: profile.emails[0].value,
+        full_name: profile.displayName,
+        hash_password: hashed_password,
+      };
+
+      const created_user = await UserDb.insert(userDetails);
+
+      return done(null, created_user);
+    })
   );
 
   return passport;
