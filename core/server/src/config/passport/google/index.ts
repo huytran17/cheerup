0beed619317de0069607e594afaa16dda1a32a4c
huytran <huytv@mor.com.vn>
2023-03-29 14:24:54 +0700

    [Draft] passport google oauth2

--- core/server/src/config/passport/google/index.ts
+++ core/server/src/config/passport/google/index.ts
@@ -1,7 +1,12 @@
-import passport_fb, {
+import { Request } from "express";
+import passport_google_oauth2, {
   StrategyOptionsWithRequest,
+  VerifyCallback,
 } from "passport-google-oauth2";
 import { PassportStatic } from "passport";
+import { UserDb } from "../../../data-access";
+import { hashPassword } from "../../password";
+import randomString from "randomstring";
 
 export default function initializeGoogle(
   passport: PassportStatic,
@@ -11,24 +16,48 @@ export default function initializeGoogle(
     callbackURL: string;
   }
 ): PassportStatic {
-  const FacebookStrategy = passport_fb.Strategy;
+  const GoogleStrategy = passport_google_oauth2.Strategy;
 
   const opts: StrategyOptionsWithRequest = {
     ...options,
     scope: ["email", "profile"],
-    successRedirect: "/api/auth/google/success",
-    failureRedirect: "/api/auth/google/failure",
     passReqToCallback: true,
   };
 
   passport.use(
-    "facebook",
-    new FacebookStrategy(opts, async function (
-      accessToken,
-      refreshToken,
-      profile,
-      cb
-    ) {})
+    "google",
+    new GoogleStrategy(opts, async function (
+      req: Request,
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done: VerifyCallback
+    ) {
+      const exist = await UserDb.findByEmail({
+        email: profile.emails[0].value,
+        deleted_at: { $in: [null, undefined] },
+      });
+
+      if (exist) {
+        return done(null, exist);
+      }
+
+      const random_password = randomString.generate();
+      const hashed_password = await hashPassword({
+        password: random_password,
+        password_confirmation: random_password,
+      });
+
+      const userDetails = {
+        email: profile.emails[0].value,
+        full_name: profile.displayName,
+        hash_password: hashed_password,
+      };
+
+      const created_user = await UserDb.insert(userDetails);
+
+      return done(null, created_user);
+    })
   );
 
   return passport;
