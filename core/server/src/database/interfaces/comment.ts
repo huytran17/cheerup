0b17bd20df1cef01c8df878a1f2e171727b61b95
huytran17 <alpha.lloyd1368@gmail.com>
2023-04-02 17:35:17 +0700

    update comment schema: add guest data

--- core/server/src/database/interfaces/comment.ts
+++ core/server/src/database/interfaces/comment.ts
@@ -2,10 +2,17 @@ import User from "../entities/user";
 import Post from "../entities/post";
 import Comment from "../entities/comment";
 
+export enum CommentType {
+  MEMBER = "member",
+  GUEST = "guest",
+}
+
 export default interface IComment {
   _id: string;
   content: string;
   user: User;
+  guest: Record<string, unknown>;
+  type: CommentType;
   post: Post;
   children: Comment[];
   parent: Comment;
--- core/server/src/database/schemas/comment.ts
+++ core/server/src/database/schemas/comment.ts
@@ -8,11 +8,18 @@ import IComment from "../interfaces/comment";
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  content: { type: String, trim: true },
+  content: { type: String, trim: true, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User" },
-  post: { type: Schema.Types.ObjectId, ref: "Post" },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   parent: { type: Schema.Types.ObjectId, ref: "Comment" },
   children: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  guest: { type: Object, default: null },
+  type: {
+    type: String,
+    trim: true,
+    enum: ["member", "guest"],
+    default: "guest",
+  },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
 });
