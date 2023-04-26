aac1a27eb52fc31c334d422130a05627210280ad
huytran17 <alpha.lloyd1368@gmail.com>
2023-04-26 19:32:02 +0700

    update publish post spec

--- core/server/src/data-access/controllers/admin/post/publish-post.spec.ts
+++ core/server/src/data-access/controllers/admin/post/publish-post.spec.ts
@@ -9,15 +9,17 @@ import {
   clearDatabase,
 } from "../../../../../__tests__/jest-mongo";
 import { ExpectSingleResult } from "../../../../../__tests__/__types__/expect-types";
-import { fakePost } from "../../../../../__tests__/__mock__";
+import { fakePost, fakeUser } from "../../../../../__tests__/__mock__";
 import { logger } from "../../../../../__tests__/jest-logger";
 import { redis } from "../../../../../__tests__/jest-redis";
 import makePostDb from "../../../make-post-db";
+import makeUserDb from "../../../make-user-db";
 import makeSubscriptionDb from "../../../make-subscription-db";
-import { PostModel, SubscriptionModel } from "../../../models";
+import { PostModel, SubscriptionModel, UserModel } from "../../../models";
 import makeCreatePost from "../../../../use-cases/post/create-post";
 import makeGetPost from "../../../../use-cases/post/get-post";
 import makeUpdatePost from "../../../../use-cases/post/update-post";
+import makeCreateUser from "../../../../use-cases/user/create-user";
 import makeGetActivatingSubscriptions from "../../../../use-cases/subscription/get-activating-subscriptions";
 import makePublishPostController from "./publish-post";
 import { HttpStatusCode } from "../../../../constants/http-status-code";
@@ -41,11 +43,16 @@ describe("publishPost", () => {
       postDbModel: PostModel,
       moment,
     });
+    const userDb = makeUserDb({
+      userDbModel: UserModel,
+      moment,
+    });
     const subscriptionDb = makeSubscriptionDb({
       subscriptionDbModel: SubscriptionModel,
       moment,
     });
 
+    const createUser = makeCreateUser({ userDb });
     const createPost = makeCreatePost({ postDb });
     const getPost = makeGetPost({ postDb, logger });
     const updatePost = makeUpdatePost({ postDb });
@@ -55,9 +62,13 @@ describe("publishPost", () => {
     });
 
     const mock_post_data = fakePost();
+    const mock_user_details = fakeUser();
 
     const created_post = await createPost({
       postDetails: mock_post_data,
+    });
+    const created_user = await createUser({
+      userDetails: mock_user_details,
     });
 
     const publishPostController = makePublishPostController({
@@ -73,6 +84,7 @@ describe("publishPost", () => {
     const request = {
       context: {
         validated: created_post,
+        user: created_user,
       },
     };
 
