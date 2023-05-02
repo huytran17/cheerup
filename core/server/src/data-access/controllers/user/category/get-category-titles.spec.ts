21ef1c2f0a044b8203ed02752dd327ec9741fd65
huytran17 <alpha.lloyd1368@gmail.com>
2023-05-02 16:58:46 +0700

    unit test: cleanup code

--- core/server/src/data-access/controllers/user/category/get-category-titles.spec.ts
+++ core/server/src/data-access/controllers/user/category/get-category-titles.spec.ts
@@ -4,7 +4,6 @@ import {
   clearDatabase,
 } from "../../../../../__tests__/jest-mongo";
 import { fakeCategory } from "../../../../../__tests__/__mock__";
-import { logger } from "../../../../../__tests__/jest-logger";
 import { redis } from "../../../../../__tests__/jest-redis";
 import makeCategoryDb from "../../../make-category-db";
 import { CategoryModel } from "../../../models";
@@ -30,7 +29,7 @@ describe("getCategoryTitles", () => {
       moment,
     });
 
-    const getCategoryTitles = makeGetCategoryTitles({ categoryDb, logger });
+    const getCategoryTitles = makeGetCategoryTitles({ categoryDb });
     const createCategory = makeCreateCategory({ categoryDb });
 
     const mock_category_data = fakeCategory();
