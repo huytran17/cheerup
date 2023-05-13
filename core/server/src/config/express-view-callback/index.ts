d03389ac6997dd8f3dd227b0ae17d82c374b1eed
huytran17 <alpha.lloyd1368@gmail.com>
2023-05-13 16:41:40 +0700

    make express view callback

---
+++ core/server/src/config/express-view-callback/index.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import _ from "lodash";
+
+type IController = (httpRequest: any) => any;
+
+export default function makeExpressViewCallback(controller: IController) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest = {
+      context: {
+        validated: Object.assign({}, req.body, req.params, req.query),
+        user: req.user,
+        file: req.file,
+        ip: req.clientIp,
+      },
+      query: req.query,
+      params: req.params,
+      method: req.method,
+      path: req.path,
+      headers: {
+        "Content-Type": req.get("Content-Type"),
+        Referer: req.get("referer"),
+        "User-Agent": req.get("User-Agent"),
+      },
+    };
+    controller(httpRequest)
+      .then((httpResponse: any) => {
+        httpResponse.headers && res.set(httpResponse.headers);
+        res.type("text/html");
+        res.status(httpResponse.statusCode).send(httpResponse.body);
+      })
+      .catch((errorObject: any) => {
+        res.status(errorObject.statusCode).send(errorObject.body);
+        next(errorObject);
+      });
+  };
+}
