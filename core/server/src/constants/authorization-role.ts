dc3433f7ce4fd10ee4a183c64f60e96c8e15cb7a
huytran17 <alpha.lloyd1368@gmail.com>
2023-05-14 23:20:35 +0700

    update authorization roles

--- core/server/src/constants/authorization-role.ts
+++ core/server/src/constants/authorization-role.ts
@@ -3,6 +3,7 @@ import { AdminType } from "../database/interfaces/admin";
 const AuthorizationRole = {
   ONLY_OWNER: [AdminType.Owner],
   OWNER_AND_COLLABORATOR: [AdminType.Owner, AdminType.Collaborator],
+  OWNER_AND_EDITOR: [AdminType.Owner, AdminType.Editor],
 };
 
 export default Object.freeze({
