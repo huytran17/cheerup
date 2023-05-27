66a9b3f89cf156724f6ae146dac11b57e77c1227
huytran17 <alpha.lloyd1368@gmail.com>
2023-05-27 19:37:10 +0700

    update rate limit

--- core/server/src/config/express-rate-limit/index.ts
+++ core/server/src/config/express-rate-limit/index.ts
@@ -2,7 +2,7 @@ import makeExpressRateLimit from "./make-express-rate-limit";
 
 const express_rate_limit_options = {
   windowMs: 15 * 60 * 1000,
-  max: 1500,
+  max: 2000,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many request from this IP, please try again after 15 minutes",
