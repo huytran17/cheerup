55f9348a71def7aceecf8ac54e8789b81a35a8e0
huytran17 <alpha.lloyd1368@gmail.com>
2023-05-28 15:37:23 +0700

    update font

--- core/user-dashboard/components/BaseNoData.vue
+++ core/user-dashboard/components/BaseNoData.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text--small text-sm-body-2 text-uppercase text-center grey--text">
+  <div class="text-body-2 text-uppercase text-center grey--text">
     <span class="app-body" v-html="$t(message)"></span>
   </div>
 </template>
