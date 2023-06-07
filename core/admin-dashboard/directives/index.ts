c7d29c7970c308f4b64fd61b1a3c0d1b269025e2
huytran17 <alpha.lloyd1368@gmail.com>
2023-06-07 23:15:26 +0700

    add page level derective for authorization

--- core/admin-dashboard/directives/index.ts
+++ core/admin-dashboard/directives/index.ts
@@ -6,10 +6,10 @@ Vue.directive("roles", {
         const user = store?.getters["auth/me"];
 
         const roles = binding.value || [];
-        const user_type = user?.type;
+        const user_type = user?.type
 
-        const invalid_role = !roles.includes(user_type);
+        const invalid_role = !roles.includes(user_type)
 
-        invalid_role && (window.location.href = vnode.context?.localePath("/403"));
+        invalid_role && (window.location.href = `${process.env.BASE_URL}/403`)
     },
 });
