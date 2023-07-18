40616165419e9290f17fb5fc598ecf434615ec9f
huytran17 <alpha.lloyd1368@gmail.com>
2023-07-18 21:12:57 +0700

    add middleware redirect-logged-user to tfa-varification page

--- core/user-dashboard/pages/auth/tfa-verification.vue
+++ core/user-dashboard/pages/auth/tfa-verification.vue
@@ -7,7 +7,7 @@ import Verify2FA from "@/components/2fa/Verify2FA";
 
 export default {
   name: "TFAVerification",
-  // middleware: ["redirect-logged-user"],
+  middleware: ["redirect-logged-user"],
   components: { Verify2FA },
 };
 </script>
