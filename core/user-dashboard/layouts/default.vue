908e02bf4e213a407acc15896e38cfb932c3fee4
huytran17 <alpha.lloyd1368@gmail.com>
2024-01-23 21:44:59 +0700

    desktop layout

--- core/user-dashboard/layouts/default.vue
+++ core/user-dashboard/layouts/default.vue
@@ -1,64 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
-        <v-row class="pb-5 pb-md-12">
-          <v-col cols="12" class="py-0">
-            <BaseAppBar />
-          </v-col>
-          <v-col cols="12" class="pt-0">
-            <BaseLanguageSwitcher />
-          </v-col>
-        </v-row>
-        <v-row class="flex-column flex-md-row flex-column-reverse">
-          <v-col cols="12" md="8">
-            <nuxt />
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-row>
-              <v-col cols="12">
-                <BaseAboutCard />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseSocialiteList />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseProfileCard />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseCategoriesCard />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseQuickAccessCard />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseSearchCard />
-              </v-col>
-              <v-col cols="12" class="mt-3">
-                <BaseQuoteCard />
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
+      <DesktopLayout />
       <v-scroll-to-top></v-scroll-to-top>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapActions } from "vuex";
 import systemMixins from "@/mixins/system";
 import authMixins from "@/mixins/auth";
-import BaseAppBar from "@/components/BaseAppBar";
-import BaseAboutCard from "@/components/about/BaseAboutCard";
-import BaseSocialiteList from "@/components/socialite/BaseSocialiteList";
-import BaseProfileCard from "@/components/user/BaseProfileCard";
-import BaseCategoriesCard from "@/components/category/BaseCategoriesCard";
-import BaseSearchCard from "@/components/searching/BaseSearchCard";
-import BaseQuickAccessCard from "@/components/quick-access/BaseQuickAccessCard";
-import BaseQuoteCard from "@/components/quote/BaseQuoteCard";
-import BaseLanguageSwitcher from "@/components/BaseLanguageSwitcher";
+import DesktopLayout from "@/components/DesktopLayout";
 
 export default {
   name: "DefaultLayout",
@@ -83,20 +36,7 @@ export default {
     };
   },
   components: {
-    BaseAppBar,
-    BaseAboutCard,
-    BaseSocialiteList,
-    BaseProfileCard,
-    BaseCategoriesCard,
-    BaseSearchCard,
-    BaseQuickAccessCard,
-    BaseQuoteCard,
-    BaseLanguageSwitcher,
-  },
-  computed: {
-    ...mapGetters({
-      category_titles: "category/category_titles",
-    }),
+    DesktopLayout,
   },
   methods: {
     ...mapActions({
@@ -107,7 +47,7 @@ export default {
   },
   async fetch() {
     try {
-      Promise.all([
+      await Promise.all([
         this.GET_LATEST_SYSTEM_CONFIGURATION(),
         this.GET_CATEGORY_TITLES(),
       ]);
@@ -117,31 +57,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-:deep(.container) {
-  max-width: 1100px !important;
-}
-
-@media only screen and (min-width: 961px) and (max-width: 1080px) {
-  :deep(.container) {
-    max-width: 990px !important;
-  }
-}
-
-@media only screen and (min-width: 768px) and (max-width: 960px) {
-  :deep(.container) {
-    width: 726px !important;
-  }
-}
-@media only screen and (max-width: 767px) {
-  :deep(.container) {
-    width: 100% !important;
-  }
-
-  :deep(#icon-container) {
-    bottom: 20px !important;
-    right: 20px !important;
-  }
-}
-</style>
