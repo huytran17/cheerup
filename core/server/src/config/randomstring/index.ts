ea4b87537cc998e534054f585bec5c18f5a081f3
huytran17 <alpha.lloyd1368@gmail.com>
2024-01-31 21:24:29 +0700

    randomstring config

---
+++ core/server/src/config/randomstring/index.ts
@@ -0,0 +1,6 @@
+import randomstring from "randomstring";
+import makeRandomString from "./make-random-string";
+
+const randomString = makeRandomString({ randomstring });
+
+export { randomString };
---
+++ core/server/src/config/randomstring/make-random-string.ts
@@ -0,0 +1,13 @@
+import Randomstring from "randomstring";
+
+const default_options = { length: 6, charset: "numeric" };
+
+export default function makeRandomString({
+  randomstring,
+}: {
+  randomstring: typeof Randomstring;
+}) {
+  return function randomString(options?: object) {
+    return randomstring.generate({ ...options, ...default_options });
+  };
+}
