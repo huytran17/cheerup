5ae0f8594f386725bde0974c018c5a165e436c4b
huytran17 <alpha.lloyd1368@gmail.com>
2024-03-31 15:53:20 +0700

    multer disk upload

--- core/server/src/config/middlewares/disk-upload-file.ts
+++ core/server/src/config/middlewares/disk-upload-file.ts
@@ -3,10 +3,25 @@ import { resolve } from "path";
 import { existsSync, mkdirSync } from "fs";
 import IUser from "../../database/interfaces/user";
 
-export default function makeDiskFileUploadMiddleware() {
+export interface IDiskUploadedFile {
+  filename: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  path: string;
+  size: number;
+}
+
+export default function makeDiskUploadFileMiddleware() {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       const user = <IUser>req.user;
+
+      if (!user) {
+        return;
+      }
+
       const file_path = resolve("src", "public", "upload", user.email);
 
       !existsSync(file_path) && mkdirSync(file_path);
@@ -14,6 +29,12 @@ export default function makeDiskFileUploadMiddleware() {
       cb(null, file_path);
     },
     filename: (req, file, cb) => {
+      const user = <IUser>req.user;
+
+      if (!user) {
+        return;
+      }
+
       cb(null, `${Date.now()}-${file.originalname}`);
     },
   });
@@ -23,5 +44,6 @@ export default function makeDiskFileUploadMiddleware() {
   return upload;
 }
 
-const disk_upload = makeDiskFileUploadMiddleware();
+const disk_upload = makeDiskUploadFileMiddleware().single("file");
+
 export { disk_upload };
