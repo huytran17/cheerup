9e089f059edba53ebc51db9a74cd62dfddcaecf7
huytran17 <alpha.lloyd1368@gmail.com>
2024-04-03 21:47:05 +0700

    use promise chaining for fs delete file

--- core/server/src/utils/delete-uploaded-file.ts
+++ core/server/src/utils/delete-uploaded-file.ts
@@ -1,5 +1,5 @@
 import { unlink } from "fs/promises";
 
 export default async function deleteUploadedFile(path: string): Promise<void> {
-  return await unlink(path);
+  return unlink(path).catch(console.error);
 }
