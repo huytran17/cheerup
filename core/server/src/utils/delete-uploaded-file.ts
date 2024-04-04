61cef46afefee2585b5ef1dae6c85ad79523ee4b
huytran17 <alpha.lloyd1368@gmail.com>
2024-04-04 20:03:24 +0700

    check file exists before del

--- core/server/src/utils/delete-uploaded-file.ts
+++ core/server/src/utils/delete-uploaded-file.ts
@@ -1,5 +1,10 @@
 import { unlink } from "fs/promises";
+import { existsSync } from "fs";
 
 export default function deleteUploadedFile(path: string): void {
+  if (!existsSync(path)) {
+    return;
+  }
+
   unlink(path).catch(console.error);
 }
