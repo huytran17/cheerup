1b8885f5a6ede0e1ea22a57f59cf8aa68a111640
huytran17 <alpha.lloyd1368@gmail.com>
2024-04-05 20:22:12 +0700

    update admin upload avatar typing

--- core/server/src/data-access/controllers/admin/admin/validators/upload-avatar.ts
+++ core/server/src/data-access/controllers/admin/admin/validators/upload-avatar.ts
@@ -1,4 +1,4 @@
-const uploadAdminAvatarRules = {
+const uploadAvatarRules = {
   _id: ["required", "regex:/^[0-9a-fA-F]{24}$/i"],
 };
-export default uploadAdminAvatarRules;
+export default uploadAvatarRules;
