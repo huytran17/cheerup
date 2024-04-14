08e3becc78ac1e3cc23d160524a596b536bfeceb
huytran17 <alpha.lloyd1368@gmail.com>
2024-04-14 20:27:15 +0700

    rmv otp-auto-focus directive

--- core/user-dashboard/directives/otp-auto-focus.ts
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-
-Vue.directive("otp-auto-focus", {
-  inserted: (el, binding, vNode) => {
-    const input = <HTMLElement>el.querySelector(".otp-input");
-    input.focus();
-  },
-});
