ee97036cba83f08c47beb121347f1b7273ff67d5
huytran17 <alpha.lloyd1368@gmail.com>
2024-05-28 22:56:26 +0700

    cleanup code

--- core/server/src/config/socket.io/handlers/make-private-admin-handler.ts
+++ core/server/src/config/socket.io/handlers/make-private-admin-handler.ts
@@ -15,44 +15,48 @@ export default function makePrivateAdminHandler({
   initialAdminNsp: InitialAdminNsp;
 }) {
   return function privateAdminHandler({ io }: { io: Server }) {
-    const online_users = {};
+    try {
+      const online_users = {};
 
-    const admin_nsp = initialAdminNsp({ io });
+      const admin_nsp = initialAdminNsp({ io });
 
-    admin_nsp.on(SocketEvents.CONNECT, (socket) => {
-      socket.on(ClientEvents.ONLINE, async ({ user_id }: IUserPayload) => {
-        const user_ids = Array.from(new Set(Object.values(online_users)));
-        if (!user_ids.includes(user_id)) {
+      admin_nsp.on(SocketEvents.CONNECT, (socket) => {
+        socket.on(ClientEvents.ONLINE, async ({ user_id }: IUserPayload) => {
+          const user_ids = Array.from(new Set(Object.values(online_users)));
+          if (!user_ids.includes(user_id)) {
+            await adminDb.update({
+              _id: user_id,
+              is_online: true,
+            });
+          }
+
+          online_users[socket.id] = user_id;
+          admin_nsp.emit(ServerEvents.ONLINE, { user_id });
+        });
+
+        socket.on(SocketEvents.DISCONNECT, async () => {
+          const user_id = online_users[socket.id];
+
+          delete online_users[socket.id];
+
+          const user_ids = Array.from(new Set(Object.values(online_users)));
+
+          const is_user_still_online = user_ids.includes(user_id);
+          if (is_user_still_online) {
+            return;
+          }
+
+          admin_nsp.emit(ServerEvents.OFFLINE, { user_id });
+
           await adminDb.update({
             _id: user_id,
-            is_online: true,
+            last_online_at: new Date(),
+            is_online: false,
           });
-        }
-
-        online_users[socket.id] = user_id;
-        admin_nsp.emit(ServerEvents.ONLINE, { user_id });
-      });
-
-      socket.on(SocketEvents.DISCONNECT, async () => {
-        const user_id = online_users[socket.id];
-
-        delete online_users[socket.id];
-
-        const user_ids = Array.from(new Set(Object.values(online_users)));
-
-        const is_user_still_online = user_ids.includes(user_id);
-        if (is_user_still_online) {
-          return;
-        }
-
-        admin_nsp.emit(ServerEvents.OFFLINE, { user_id });
-
-        await adminDb.update({
-          _id: user_id,
-          last_online_at: new Date(),
-          is_online: false,
         });
       });
-    });
+    } catch (error) {
+      console.error(error);
+    }
   };
 }
