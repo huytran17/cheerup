6fd8c8d2f4d10c2d02acea49648adfa8d4ad10f4
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-02 21:15:58 +0700

    add mime types const

---
+++ core/server/src/constants/mime-types.ts
@@ -0,0 +1,24 @@
+const MimeTypes = {
+  image: [
+    "image/apng",
+    "image/avif",
+    "image/bmp",
+    "image/gif",
+    "image/vnd.microsoft.icon",
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+    "image/tiff",
+    "image/webp",
+  ],
+  excel: [
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  ],
+};
+
+export default Object.freeze({
+  MimeTypes,
+});
+
+export { MimeTypes };
