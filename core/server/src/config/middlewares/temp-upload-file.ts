e0bcae39e6749e5286b9e9febdd6270be1dd0adb
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-02 18:41:05 +0700

    add file filter for temp upload

--- core/server/src/config/middlewares/temp-upload-file.ts
+++ core/server/src/config/middlewares/temp-upload-file.ts
@@ -1,8 +1,31 @@
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
 import { tmpdir } from "os";
 
 export default function makeTempUploadFile() {
-  const upload = multer({ dest: tmpdir() });
+  const file_filter = (
+    req: Express.Request,
+    file: Express.Multer.File,
+    cb: FileFilterCallback
+  ) => {
+    const valid_mimetypes = [
+      "application/vnd.ms-excel",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    ];
+
+    if (valid_mimetypes.includes(file.mimetype)) {
+      return cb(null, true);
+    }
+
+    cb(new Error("Invalid file format."));
+  };
+
+  const upload = multer({
+    dest: tmpdir(),
+    fileFilter: file_filter,
+    limits: {
+      fileSize: 1e6,
+    },
+  });
 
   return upload;
 }
