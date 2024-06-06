3bf0744228b175dc61d4e769c89c9dcbea86cfe9
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-06 21:01:29 +0700

    upload excel template controller

--- core/server/src/data-access/controllers/admin/v2/system-configuration/index.ts
+++ core/server/src/data-access/controllers/admin/v2/system-configuration/index.ts
@@ -2,9 +2,15 @@ import {
   getSystemConfiguration,
   updateSystemConfiguration,
 } from "../../../../../use-cases/system-configuration";
+import makeUploadExcelTemplateController from "./upload-excel-template";
 import makeUploadFolderIconController from "./upload-folder-icon";
 import makeUploadOwnerAvatarController from "./upload-owner-avatar";
 import makeUploadThumbnaiilController from "./upload-thumbnail";
+
+const uploadExcelTemplateController = makeUploadExcelTemplateController({
+  getSystemConfiguration,
+  updateSystemConfiguration,
+});
 
 const uploadThumbnaiilController = makeUploadThumbnaiilController({
   getSystemConfiguration,
@@ -25,10 +31,12 @@ export default Object.freeze({
   uploadClientAvatarController,
   uploadFolderIconController,
   uploadThumbnaiilController,
+  uploadExcelTemplateController,
 });
 
 export {
   uploadClientAvatarController,
+  uploadExcelTemplateController,
   uploadFolderIconController,
   uploadThumbnaiilController,
 };
