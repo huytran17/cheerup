078c232634ac37fdc13293bedcd7b69d4eef1adc
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-09 20:26:22 +0700

    update v2 router

--- core/server/src/routes/admin/v2/index.ts
+++ core/server/src/routes/admin/v2/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import authenticateAdminJWT from "../../../config/middlewares/authenticate-admin-jwt";
-import { diskUpload } from "../../../config/multer";
+import { diskUpload, tempUpload } from "../../../config/multer";
 import { MimeTypes } from "../../../constants/mime-types";
 import adminRouter from "./admin";
+import batchAdminRouter from "./batch/admin";
 import categoryRouter from "./category";
 import galleryRouter from "./gallery";
 import postRouter from "./post";
 import systemConfigurationRouter from "./system-configuration";
-import systemConfigurationTemplateRouter from "./system-configuration-template";
+import excelSystemConfigurationRouter from "./upload-excel/system-configuration";
 import userRouter from "./user";
 
 const adminV2Router = express.Router();
@@ -16,6 +17,10 @@ const disk_upload_image = diskUpload({ mimetypes: MimeTypes.IMAGE }).single(
   "file"
 );
 const disk_upload_excel = diskUpload({ mimetypes: MimeTypes.EXCEL }).single(
+  "file"
+);
+
+const temp_upload_excel = tempUpload({ mimetypes: MimeTypes.EXCEL }).single(
   "file"
 );
 
@@ -56,10 +61,16 @@ adminV2Router.use(
   systemConfigurationRouter
 );
 adminV2Router.use(
-  "/system-configuration-template",
+  "/upload-excel/system-configuration",
   authenticateAdminJWT(),
   disk_upload_excel,
-  systemConfigurationTemplateRouter
+  excelSystemConfigurationRouter
+);
+adminV2Router.use(
+  "/batch/admin",
+  authenticateAdminJWT(),
+  temp_upload_excel,
+  batchAdminRouter
 );
 
 export default adminV2Router;
