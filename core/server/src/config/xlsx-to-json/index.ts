7449acc7850e735619a09fd078776406cd36ae29
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-09 20:26:44 +0700

    config convert-excel-to-json pkg

--- core/server/src/config/xlsx-to-json/index.ts
+++
@@ -1,8 +0,0 @@
-import convertXlsxToJson from "convert-xlsx-to-json";
-import makeConvertXlsxToJSON from "./make-xlsx-to-json";
-
-const convertXlsxToJSON = makeConvertXlsxToJSON({
-  convertXlsxToJson,
-});
-
-export { convertXlsxToJSON };
