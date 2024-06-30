17ad3052d67214e2ec0a5f25661e4afd407f9445
huytran17 <alpha.lloyd1368@gmail.com>
2024-06-30 18:20:24 +0700

    admin-dashboard: update default layout to remove nav btn

--- core/admin-dashboard/layouts/default.vue
+++ core/admin-dashboard/layouts/default.vue
@@ -3,13 +3,6 @@
     <v-main class="background">
       <TheSideNav />
       <v-container fluid>
-        <v-row class="pt-2 pb-4">
-          <v-col cols="12">
-            <v-icon color="black" @click="$router.back()"
-              >mdi-keyboard-backspace</v-icon
-            >
-          </v-col>
-        </v-row>
         <nuxt />
       </v-container>
       <v-scroll-to-top></v-scroll-to-top>
