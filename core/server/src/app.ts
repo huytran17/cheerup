b6e48c6ef2f08b1a8753a1c02473781e50c6355f
huytran17 <alpha.lloyd1368@gmail.com>
2024-07-14 21:01:47 +0700

    rmv json2xls from server

--- core/server/src/app.ts
+++ core/server/src/app.ts
@@ -8,7 +8,6 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import { createServer } from "http";
-import json2xls from "json2xls";
 import { join } from "path";
 import requestIp from "request-ip";
 import responseTime from "response-time";
@@ -35,7 +34,6 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(compression());
 app.use(responseTime());
-app.use(json2xls.middleware);
 app.use(
   "/upload",
   express.static(join(__dirname, "upload"), {
--- core/server/src/config/json2xls/index.ts
+++
@@ -1,6 +0,0 @@
-import makeJsonToXls from "./make-json-to-xls";
-import json2xls from "json2xls";
-
-const jsonToXls = makeJsonToXls({ json2xls });
-
-export { jsonToXls };
