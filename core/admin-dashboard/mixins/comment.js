2cd86ce5d1d4c4c2a09b89745c6e899734db03af
huytran17 <alpha.lloyd1368@gmail.com>
2024-08-29 20:28:09 +0700

    update comment mixin

--- core/admin-dashboard/mixins/comment.js
+++ core/admin-dashboard/mixins/comment.js
@@ -1,20 +1,23 @@
-import { mapActions, mapMutations, mapGetters } from "vuex";
+import { mapActions, mapGetters, mapMutations } from "vuex";
 
 export default {
   computed: {
     ...mapGetters({
       comment: "comment/comment",
       comments: "comment/comments",
+      comment_pagination: "comment/pagination",
     }),
   },
   methods: {
     ...mapActions({
       GET_COMMENTS: "comment/GET_COMMENTS",
       HARD_DELETE_COMMENT: "comment/HARD_DELETE_COMMENT",
+      GET_COMMENTS_PAGINATED: "comment/GET_COMMENTS_PAGINATED",
     }),
     ...mapMutations({
       SET_COMMENT: "comment/SET_COMMENT",
       SET_COMMENTS: "comment/SET_COMMENTS",
+      SET_COMMENT_PAGINATION: "comment/SET_COMMENT_PAGINATION",
     }),
   },
 };
