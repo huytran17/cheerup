f2bacc5d92122bf166070bcf32b43074196f93a4
huytran17 <alpha.lloyd1368@gmail.com>
2024-09-06 19:42:36 +0700

    add get subscription paginated to subscription mixin

--- core/admin-dashboard/mixins/subscription.js
+++ core/admin-dashboard/mixins/subscription.js
@@ -1,4 +1,4 @@
-import { mapActions, mapMutations, mapGetters } from "vuex";
+import { mapActions, mapGetters, mapMutations } from "vuex";
 
 export default {
   computed: {
@@ -6,16 +6,22 @@ export default {
       subscription: "subscription/subscription",
       subscriptions: "subscription/subscriptions",
       subscription_analys_data: "subscription/subscription_analys_data",
+      subscription_pagination: "subscription/pagination",
     }),
   },
   methods: {
     ...mapActions({
       GET_SUBSCRIPTIONS: "subscription/GET_SUBSCRIPTIONS",
       GET_SUBSCRIPTION_ANALYTICS: "subscription/GET_SUBSCRIPTION_ANALYTICS",
+      GET_SUBSCRIPTIONS_PAGINATED: "subscription/GET_SUBSCRIPTIONS_PAGINATED",
     }),
+
     ...mapMutations({
       SET_SUBSCRIPTION: "subscription/SET_SUBSCRIPTION",
       SET_SUBSCRIPTIONS: "subscription/SET_SUBSCRIPTIONS",
+      SET_SUBSCRIPTION_PAGINATION: "subscription/SET_SUBSCRIPTION_PAGINATION",
+      UPDATE_SUBSCRIPTION_PAGINATION:
+        "subscription/UPDATE_SUBSCRIPTION_PAGINATION",
     }),
   },
 };
