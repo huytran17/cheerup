9ff244dece08ee3cddd36fbcbab10cbddb077323
huytran17 <alpha.lloyd1368@gmail.com>
2025-07-12 13:15:58 +0700

    add vuetify plugin

--- core/user-dashboard-v2/nuxt.config.ts
+++ core/user-dashboard-v2/nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@pinia/nuxt"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    "@nuxt/image",
+    "@pinia/nuxt",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 });
