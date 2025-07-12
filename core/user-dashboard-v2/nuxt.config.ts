e9fb480e3a1977c573a5e809fa71a0d296dead7c
huytran17 <alpha.lloyd1368@gmail.com>
2025-07-12 14:38:54 +0700

    ignore component path prefix

--- core/user-dashboard-v2/nuxt.config.ts
+++ core/user-dashboard-v2/nuxt.config.ts
@@ -4,6 +4,12 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   build: {
     transpile: ["vuetify"],
   },
